cffcc04bce2100903ff3a001b39b4c8a8dd89144
Jungjaewon <Woodcook486@naver.com>
2022-10-16 21:21:12 +0900

    Time: 38 ms (99.09%), Space: 14.7 MB (41.24%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,6 +1,5 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        import re
-        s = re.sub('[^0-9a-zA-Z]', '', s).lower()
+        s = ''.join(filter(str.isalnum, s)).lower()
         return s == s[::-1]
         
