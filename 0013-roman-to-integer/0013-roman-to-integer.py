d2d007fe944e2901db64bb0f75f5e6291bee021f
Jungjaewon <Woodcook486@naver.com>
2022-10-16 21:14:10 +0900

    Time: 90 ms (51.24%), Space: 13.9 MB (30.45%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -0,0 +1,40 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman2integer_dict = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000,
+        }
+        ans, total, i = 0, len(s), 0
+        
+        while i < total:
+            
+            if i+1 < total and s[i] == 'I' and s[i + 1] == 'V':
+                    ans += 4
+                    i += 2
+            elif i+1 < total and s[i] == 'I' and s[i + 1] == 'X':
+                    ans += 9
+                    i += 2
+            elif i+1 < total and s[i] == 'X' and s[i + 1] == 'L':
+                    ans += 40
+                    i += 2
+            elif i+1 < total and s[i] == 'X' and s[i + 1] == 'C':
+                ans += 90
+                i += 2
+            elif i+1 < total and s[i] == 'C' and s[i + 1] == 'D':
+                    ans += 400
+                    i += 2
+            elif i+1 < total and s[i] == 'C' and s[i + 1] == 'M':
+                    ans += 900
+                    i += 2
+            else:
+                ans += roman2integer_dict[s[i]]
+                i += 1
+            
+            
+        
+        return ans
