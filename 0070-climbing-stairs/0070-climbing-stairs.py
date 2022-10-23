0541f600d4b78ccadc4642e0109cbda044d19a1b
Jungjaewon <Woodcook486@naver.com>
2022-10-23 22:07:22 +0900

    Time: 66 ms (10.21%), Space: 14 MB (11.89%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,7 +1,7 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
         s_dict = {1: 1, 2: 2}
-        for i in range(3, 46):
+        for i in range(3, n + 1):
             s_dict[i] = s_dict[i - 1] + s_dict[i - 2]
-            if n in s_dict:
-                return s_dict[n]
+        if n in s_dict:
+            return s_dict[n]
