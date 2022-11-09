859d776353e28acc2231a2ef35ad9f8cb5b556fe
Jungjaewon <Woodcook486@naver.com>
2022-11-09 14:31:14 +0900

    Time: 7611 ms (5.00%), Space: 22.5 MB (5.45%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.py
@@ -0,0 +1,29 @@
+import queue
+import copy
+
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        Q, ans = queue.Queue(), list()
+        ret = list()
+        
+        for c in candidates:
+            Q.put([c])
+            
+        while Q.qsize():
+            temp = Q.get()
+            temp_sum = sum(temp)
+            
+            if temp_sum == target:
+                ans.append(temp)
+            else:
+                for c in candidates:
+                    if not temp_sum + c > target:
+                        temp_copy = copy.deepcopy(temp)
+                        temp_copy.append(c)
+                        Q.put(temp_copy)
+                        
+        ans = [sorted(x) for x in ans]
+        for candi in ans:
+            if candi not in ret:
+                ret.append(list(candi))
+        return ret
