cf2c3995eb65accfab717a38411dc0ff2e8c0759
Jungjaewon <Woodcook486@naver.com>
2022-11-12 11:46:19 +0900

    Time: 27 ms (97.95%), Space: 14 MB (5.27%) - LeetHub

---
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -0,0 +1,26 @@
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        visited, ans = [[False] * len(matrix[0]) for _ in range(len(matrix))], list()
+        m, n = len(matrix), len(matrix[0])
+        dir_dict = {0 : [0,1], 1 : [1, 0], 2 : [0, -1], 3 : [-1, 0]}
+        ch_dict = {0 : 1, 1 : 2, 2 :3, 3: 0}
+        cur_dir, x, y, cnt = 0, 0, 0, 0
+        
+        while cnt < m * n:
+            #print(f'x : {x}, y : {y}')
+            visited[x][y] = True
+            ans.append(matrix[x][y])
+            
+            dx, dy = dir_dict[cur_dir]
+            nx, ny = x + dx, y + dy
+            
+            if -1 < nx and nx < m and -1 < ny and ny < n and not visited[nx][ny]:
+                x, y = nx, ny
+            else:
+                cur_dir = ch_dict[cur_dir]
+                dx, dy = dir_dict[cur_dir]
+                x, y = x + dx, y + dy
+            
+            cnt += 1
+        return ans
+        
