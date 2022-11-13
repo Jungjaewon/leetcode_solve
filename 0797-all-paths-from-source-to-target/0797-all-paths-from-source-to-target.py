c9cf07e7aa4c65eb2631fdf782b403b463fb52bf
Jungjaewon <Woodcook486@naver.com>
2022-11-13 12:16:58 +0900

    Time: 229 ms (37.50%), Space: 15.7 MB (80.53%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.py
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.py
@@ -9,10 +9,22 @@ class Solution:
             else:
                 for dest in graph[node]:
                     if not visited[dest]:
-                        visited[dest] = True
+                        #visited[dest] = True
                         history.append(dest)
                         dfs(dest)
-                        visited[dest] = False
+                        #visited[dest] = False
                         del history[-1]
         dfs(0)
         return ans
+    #def allPathsSourceTarget(self, g, cur=0):
+    #    if cur == len(g) - 1: return [[len(g) - 1]]
+    #    return [([cur] + path) for i in g[cur] for path in self.allPathsSourceTarget(g, i)]
+    #def allPathsSourceTarget(self, graph):
+    #    def dfs(cur, path):
+    #        if cur == len(graph) - 1: res.append(path)
+    #        else:
+    #            for i in graph[cur]: dfs(i, path + [i])
+    #    res = []
+    #    dfs(0, [0])
+    #    return res
+    # https://leetcode.com/problems/all-paths-from-source-to-target/discuss/118691/C%2B%2BPython-Backtracking
