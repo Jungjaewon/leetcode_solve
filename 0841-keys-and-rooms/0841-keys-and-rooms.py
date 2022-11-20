6698dad611940456366108c1e5ee77e1adca0ba1
정재원 <Woodcook486@naver.com>
2022-11-20 14:38:57 +0900

    Time: 146 ms (44.97%), Space: 15 MB (13.38%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.py
+++ 0841-keys-and-rooms/0841-keys-and-rooms.py
@@ -1,6 +1,17 @@
 class Solution:
     def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
         visited = [False] * len(rooms)
+        
+        def helper(room_n):
+            visited[room_n] = True
+            for n in rooms[room_n]:
+                if not visited[n]:
+                    helper(n)
+        helper(0)
+        return all(visited)
+        
+        """
+        visited = [False] * len(rooms)
         visited[0] = True
         temp = rooms[0]
         while len(temp):
@@ -12,3 +23,4 @@ class Solution:
                         temp_l.add(k)
             temp = list(temp_l)
         return all(visited)
+        """
