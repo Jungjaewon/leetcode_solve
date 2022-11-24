e72fa6ca121012ae4187d183462dfbe95104387d
정재원 <Woodcook486@naver.com>
2022-11-24 23:55:45 +0900

    Time: 39 ms (91.01%), Space: 13.9 MB (79.57%) - LeetHub

--- 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.py
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.py
@@ -1,5 +1,37 @@
 class Solution:
     def removeOccurrences(self, s: str, part: str) -> str:
+        temp, idx = "", 0
+        while idx < len(s):
+            temp += s[idx]
+            if part in temp:
+                temp = temp.replace(part, "")
+            idx += 1
+        return temp
+        #Ref:https://leetcode.com/problems/remove-all-occurrences-of-a-substring/discuss/1373251/PYTHON-FOR-BEGINNERS
+        """
         while part in s:
             s = s.replace(part, '', 1)
         return s
+        """
+        """
+        ### Time Limit
+        while part in s:
+            ans, idx = list(), 0
+            while idx < len(s):
+                #print(f'{part}, {s[idx:idx+ len(part)]}, {part == s[idx:idx+ len(part)]}')
+                if part == s[idx:idx + len(part)]:
+                    idx += len(part)
+                else:
+                    #print(f'false idx : {idx}')
+                    ans.append(s[idx])
+                    idx += 1
+                if len(ans):
+                    for c in s:
+                        ans.append(c)
+                    s = ''.join(ans)
+                    ans, idx = list(), 0
+            s = ''.join(ans)
+            #print(s)
+        return s
+        
+        """
