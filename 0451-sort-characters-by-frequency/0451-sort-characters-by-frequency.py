28a3e9335e06abd24c6ccf75ea2e4979cbfacca9
정재원 <Woodcook486@naver.com>
2022-12-02 19:12:24 +0900

    Time: 34 ms (99.34%), Space: 15.3 MB (49.74%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
@@ -3,9 +3,16 @@ class Solution:
         from collections import Counter
         result_cnt, s = Counter(s), ""
         for x in result_cnt.most_common():
+            s += x[0] * x[1]
+        return s
+        """
+        from collections import Counter
+        result_cnt, s = Counter(s), ""
+        for x in result_cnt.most_common():
             for _ in range(x[1]):
                 s += x[0]
         return s
+        """
         """
         from collections import defaultdict
         ans_dict, ans = defaultdict(int), list()
