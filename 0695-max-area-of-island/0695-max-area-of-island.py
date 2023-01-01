8844e6302c53f05765f50621c1071da589942f0a
정재원 <Woodcook486@naver.com>
2023-01-01 19:29:11 +0900

    Time: 131 ms (94.49%), Space: 17.5 MB (37.38%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -1,5 +1,24 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        dir_xy, ans = [[0,1],[0,-1],[1,0],[-1,0]], [-10e4]
+        m, n  = len(grid), len(grid[0])
+        visited = [[False] * n for _ in range(m)]
+        def dfs(x,y):
+            temp_area[0] += 1
+            visited[x][y] = True
+            for dx, dy in dir_xy:
+                nx, ny = x + dx, y + dy
+                if -1 < nx and nx < m and -1 < ny and ny < n and not visited[nx][ny] and grid[nx][ny] == 1:
+                    dfs(nx, ny)
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1 and visited[i][j] == False:
+                    visited[i][j], temp_area = True, [0]
+                    dfs(i, j)
+                    ans[0] = max(ans[0], temp_area[0])
+        return ans[0] if ans[0] != -10e4 else 0
+                    
+        """
         from queue import Queue
         ans = [-10e4]
         dir_xy = [[0,1],[0,-1],[1,0],[-1,0]]
@@ -21,4 +40,4 @@ class Solution:
                                 visited[nx][ny] = True
                     ans[0] = max(ans[0], temp_area)
         return ans[0] if ans[0] != -10e4 else 0
-        
+        """
