07df2b6cda067830fa2ed5907e9ae2c37de3b496
정재원 <Woodcook486@naver.com>
2023-01-02 23:15:14 +0900

    Time: 1342 ms (62.02%), Space: 32.3 MB (99.64%) - LeetHub

--- 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.py
+++ 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.py
@@ -1,8 +1,31 @@
 class Solution:
     def garbageCollection(self, garbage: List[str], travel: List[int]) -> int:
+        ans = [0]
+        def collect_gar(type_g):
+            last_idx = -1
+            for idx, cnt_dict in enumerate(garbage):
+                if type_g in cnt_dict:
+                    last_idx = idx
+            if last_idx == -1:
+                return
+            else:
+                for idx, s in enumerate(garbage):
+                    if type_g in s:
+                        new_s = s.replace(type_g, '')
+                        ans[0] += len(s) - len(new_s)
+                        garbage[idx] = new_s
+                    
+                    if last_idx == idx:
+                        break
+                    elif idx + 1 != len(garbage):
+                        ans[0] += travel[idx]
+        collect_gar('P')
+        collect_gar('G')
+        collect_gar('M')
+        return ans[0]
+        """
         from collections import Counter
         garbage, ans = [ dict(Counter(s)) for s in garbage], [0]
-        #print(garbage)
         def collect_gar(type_g):
             last_idx = -1
             for idx, cnt_dict in enumerate(garbage):
@@ -14,7 +37,6 @@ class Solution:
                 for idx, cnt_dict in enumerate(garbage):
                     if type_g in cnt_dict:
                         ans[0] += cnt_dict[type_g]
-                        #print('collect : ', ans[0], idx)
                         del cnt_dict[type_g]
                     
                     if idx == last_idx:
@@ -24,10 +46,8 @@ class Solution:
                         #print('move : ', ans[0], idx)
 
         collect_gar('P')
-        #print(ans[0])
         collect_gar('G')
-        #print(ans[0])
         collect_gar('M')
-        #print(ans[0])
         return ans[0]
+        """
         
