7fdfd1999b921dcf4815f6fe13a6d0f090e2fd97
정재원 <Woodcook486@naver.com>
2023-01-03 18:28:31 +0900

    Time: 64 ms (16.45%), Space: 13.8 MB (95.69%) - LeetHub

---
+++ 2396-strictly-palindromic-number/2396-strictly-palindromic-number.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isStrictlyPalindromic(self, n: int) -> bool:
+        def numberToBase(n, b):
+            if n == 0:
+                return '0'
+            digits = []
+            while n:
+                digits.append(int(n % b))
+                n //= b
+            return ''.join([str(x) for x in digits[::-1]])
+        for b in range(2, n - 2 + 1):
+            s_n = numberToBase(n, b)
+            if s_n != s_n[::-1]:
+                return False
+        return True
