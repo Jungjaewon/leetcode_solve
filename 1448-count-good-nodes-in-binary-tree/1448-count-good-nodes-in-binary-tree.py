c87f4cd0b5f21f3b7b22355197eb5b708a322322
정재원 <Woodcook486@naver.com>
2023-01-04 23:00:23 +0900

    Time: 642 ms (29.71%), Space: 32.4 MB (86.55%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
@@ -6,6 +6,16 @@
 #         self.right = right
 class Solution:
     def goodNodes(self, root: TreeNode) -> int:
+        def helper(node, val):
+            if node is None:
+                return 0
+            s = 1 if node.val >= val else 0
+            max_v = max(node.val, val)
+            
+            return s + helper(node.left, max_v) + helper(node.right, max_v)
+        return helper(root, root.val)
+        # https://leetcode.com/problems/count-good-nodes-in-binary-tree/discuss/2511509/Python-easy-to-understand-solution
+        """
         ans = [0]
         def helper(node:TreeNode, path: List[int]):
             if node is None:
@@ -25,3 +35,4 @@ class Solution:
             path.pop()
         helper(root, [])
         return ans[0]
+        """
