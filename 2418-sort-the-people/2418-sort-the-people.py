91a80896a24452585503ddfed557376d5825bc1b
정재원 <Woodcook486@naver.com>
2023-01-16 21:23:45 +0900

    Time: 97 ms (100.00%), Space: 14.3 MB (99.46%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -1,5 +1,6 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        ans_list = [[name, height] for name, height in zip(names, heights)]
-        ans_list.sort(key=lambda x:x[1], reverse=True)
-        return [x[0] for x in ans_list]
+        #ans_list = [[name, height] for name, height in zip(names, heights)]
+        #ans_list.sort(key=lambda x:x[1], reverse=True)
+        #return [x[0] for x in ans_list]
+        return [x[0] for x in sorted([[name, height] for name, height in zip(names, heights)], key=lambda x:x[1])][::-1]
