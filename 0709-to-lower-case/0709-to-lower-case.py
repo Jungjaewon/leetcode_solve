e04d735d4e66ea2974131e141a0026c7feb86acc
정재원 <Woodcook486@naver.com>
2023-01-17 18:37:38 +0900

    Time: 32 ms (77.71%), Space: 13.8 MB (45.98%) - LeetHub

--- 0709-to-lower-case/0709-to-lower-case.py
+++ 0709-to-lower-case/0709-to-lower-case.py
@@ -1,3 +1,4 @@
 class Solution:
     def toLowerCase(self, s: str) -> str:
-        return s.lower()
+        #return s.lower()
+        return "".join([ chr(ord(c) + 32) if 65 <= ord(c) and ord(c) <= 90 else c for c in s])
