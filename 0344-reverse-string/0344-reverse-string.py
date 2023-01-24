973744a965cb68726a92ca4328c07271b73f7518
정재원 <Woodcook486@naver.com>
2023-01-24 11:24:55 +0900

    Time: 208 ms (63.83%), Space: 18.3 MB (81.58%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -6,18 +6,20 @@ class Solution:
         for _ in range(len(s) // 2):
             s.pop(0)
         """
+        """
         s_i, e_i = 0, len(s) - 1
         while True:
-            print(s_i, e_i)
             s[s_i], s[e_i] = s[e_i], s[s_i]
-            
             if s_i == e_i:
                 break
             elif len(s) & 1 == 0 and abs(s_i - e_i) == 1:
                 break
-            
             s_i, e_i = s_i + 1, e_i - 1
-            
+        """
+        s_i, e_i = 0, len(s) - 1
+        while s_i < e_i:
+            s[s_i], s[e_i] = s[e_i], s[s_i]
+            s_i, e_i = s_i + 1, e_i - 1
         """
         Do not return anything, modify s in-place instead.
         """
