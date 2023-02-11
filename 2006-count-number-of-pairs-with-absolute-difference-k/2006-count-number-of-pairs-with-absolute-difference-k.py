b9dbab5a665f60b3d3d011a9810ca7aa15321102
정재원 <Woodcook486@naver.com>
2023-02-11 21:52:59 +0900

    Time: 56 ms (98.37%), Space: 13.8 MB (59.50%) - LeetHub

--- 2006-count-number-of-pairs-with-absolute-difference-k/2006-count-number-of-pairs-with-absolute-difference-k.py
+++ 2006-count-number-of-pairs-with-absolute-difference-k/2006-count-number-of-pairs-with-absolute-difference-k.py
@@ -17,15 +17,25 @@ class Solution:
             ans += seen[n - k] + seen[n + k]
         return ans
         """
+        """
         arr = [0] * 201
         count = 0
         
         # Put the count of each number in the array above
-        for num in nums: arr[num] += 1
+        for num in nums: 
+            arr[num] += 1
         
         # Now for each number, just check how many times (num + k) appears in the array and 
 		# increment the counter variable with that value
 		# Because for each number, if we have number + k in array, it means that pair has an absolute difference of k
-        for num in nums: count += arr[num + k]
+        for num in nums: 
+            count += arr[num + k]
 
         return count
+        """
+        ans, seen = 0, defaultdict(int)
+        for n in nums:
+            seen[n] += 1
+        for n in nums:
+            ans += seen[n + k]
+        return ans
