dc56770201100a21ddd3dcb9d3e580afd2e47a32
정재원 <Woodcook486@naver.com>
2023-02-12 22:52:03 +0900

    Time: 33 ms (75.69%), Space: 13.9 MB (58.95%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -1,6 +1,6 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        
+        """
         if numRows == 1:
             return [[1]]
         elif numRows == 2:
@@ -13,3 +13,14 @@ class Solution:
                     temp.append(ans[-1][j] + ans[-1][j + 1])
                 ans.append([1, *temp, 1])
             return ans
+        """
+        if numRows >= 3:
+            ans = [[1],[1,1]]
+            for i in range(numRows - 2):
+                temp = list()
+                for j in range(len(ans[-1]) - 1):
+                    temp.append(ans[-1][j] + ans[-1][j + 1])
+                ans.append([1, *temp, 1])
+            return ans
+        else:
+            return [[1]] if numRows == 1 else [[1],[1,1]]
