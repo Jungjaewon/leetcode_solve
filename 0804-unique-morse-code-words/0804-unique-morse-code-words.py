85e6c308409dddb4a03cf2a2c2cce962cdc59e1f
정재원 <Woodcook486@naver.com>
2023-02-19 12:36:44 +0900

    Time: 36 ms (72.17%), Space: 13.8 MB (97.77%) - LeetHub

--- 0804-unique-morse-code-words/0804-unique-morse-code-words.py
+++ 0804-unique-morse-code-words/0804-unique-morse-code-words.py
@@ -1,18 +1,28 @@
 class Solution:
     def uniqueMorseRepresentations(self, words: List[str]) -> int:
+        """
         morse = [".-","-...","-.-.","-..",".","..-.","--.","....",
                  "..",".---","-.-",".-..","--","-.","---",".--.","--.-",
                  ".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
         result = set()
         def convert(c):
             return ord(c) - ord('a')
-        
         for word in words:
             transed = list()
             for c in word:
                 idx = convert(c)
                 transed.append(morse[idx])
             result.add(''.join(transed))
-        
+        return len(result)
+        """
+        morse = [".-","-...","-.-.","-..",".","..-.","--.","....",
+                 "..",".---","-.-",".-..","--","-.","---",".--.","--.-",
+                 ".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+        result = set()
+        for word in words:
+            transed = list()
+            for c in word:
+                transed.append(morse[ord(c) - ord('a')])
+            result.add(''.join(transed))
         return len(result)
                 
