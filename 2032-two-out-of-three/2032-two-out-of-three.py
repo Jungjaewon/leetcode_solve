7a8fb740f77731adcc4786cc4e9de571d479af97
정재원 <Woodcook486@naver.com>
2023-02-20 22:11:36 +0900

    Time: 74 ms (70.26%), Space: 13.9 MB (73.37%) - LeetHub

---
+++ 2032-two-out-of-three/2032-two-out-of-three.py
@@ -0,0 +1,12 @@
+class Solution:
+    def twoOutOfThree(self, nums1: List[int], nums2: List[int], nums3: List[int]) -> List[int]:
+        check_dict = defaultdict(int)
+        def put_num(num_list):
+            for n in set(num_list):
+                check_dict[n] += 1
+        
+        put_num(nums1)
+        put_num(nums2)
+        put_num(nums3)
+        
+        return [key for key, value in check_dict.items() if value >= 2]
