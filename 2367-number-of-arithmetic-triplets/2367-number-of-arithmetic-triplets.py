8d2ece9525c3d807e7b6224adcac63a8f37ad68f
정재원 <Woodcook486@naver.com>
2023-02-23 19:44:21 +0900

    Time: 79 ms (37.01%), Space: 13.9 MB (12.29%) - LeetHub

--- 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.py
+++ 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.py
@@ -9,6 +9,7 @@ class Solution:
                         ans += 1
         return ans
         """
+        """
         ans, temp_set = 0, set()
         for i in range(len(nums)):
             for j in range(i + 1, len(nums)):
@@ -16,6 +17,17 @@ class Solution:
                     temp_set.add(f'{i}_{j}')
         for s in temp_set:
             i, j = [int(x) for x in s.split('_')]
+            for k in range(j + 1, len(nums)):
+                if nums[k] - nums[j] == diff:
+                    ans += 1
+        return ans
+        """
+        ans, temp_set = 0, set()
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[j] - nums[i] == diff:
+                    temp_set.add(j)
+        for j in temp_set:
             for k in range(j + 1, len(nums)):
                 if nums[k] - nums[j] == diff:
                     ans += 1
