7a08321df8e67151a9d273439902199893dce3c5
정재원 <Woodcook486@naver.com>
2023-02-24 23:10:11 +0900

    Time: 33 ms (62.20%), Space: 13.9 MB (6.21%) - LeetHub

--- 1844-replace-all-digits-with-characters/1844-replace-all-digits-with-characters.py
+++ 1844-replace-all-digits-with-characters/1844-replace-all-digits-with-characters.py
@@ -1,8 +1,16 @@
 class Solution:
     def replaceDigits(self, s: str) -> str:
+        """
         s_list = list(s)
         for i in range(1, len(s_list)):
             c = s_list[i]
             if '0' <= c and c <= '9':
                 s_list[i] = chr(ord(s_list[i - 1]) + int(c))
         return ''.join(s_list)
+        """
+        s_list = list(s)
+        for i in range(1, len(s_list)):
+            if '0' <= s_list[i] and s_list[i] <= '9':
+                s_list[i] = chr(ord(s_list[i - 1]) + int(s_list[i]))
+        return ''.join(s_list)
+        
