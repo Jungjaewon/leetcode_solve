e171fe324c6a384bd34d57b81fb02a35b5b8b103
정재원 <Woodcook486@naver.com>
2023-02-25 16:40:51 +0900

    Time: 1602 ms (5.01%), Space: 48.8 MB (97.02%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.py
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.py
@@ -20,7 +20,7 @@ class Solution:
         func(1, root)
         return ans[0] if ans[0] != 10e4 else 0
         """
-        
+        """
         if root is None:
             return 0
         else:
@@ -33,3 +33,27 @@ class Solution:
                 else:
                     return min(func(depth + 1, node.left), func(depth + 1, node.right))
             return func(1, root)
+        """
+        from queue import Queue
+        Q, ans = Queue(), 10e4
+        
+        if root is None:
+            return 0
+        else:
+            depth = 1
+            Q.put(root)
+            while Q.qsize():
+                for _ in range(Q.qsize()):
+                    node = Q.get()
+                    if node.left is None and node.right is None:
+                        ans = min(ans, depth)
+
+                    if node.left:
+                        Q.put(node.left)
+                    if node.right:
+                        Q.put(node.right)
+                depth += 1
+            return ans
+                
+                
+                
