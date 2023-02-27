cfb1e4094ccebf7d5bdd64491b7c9ee39001984a
정재원 <Woodcook486@naver.com>
2023-02-28 00:27:00 +0900

    Time: 31 ms (73.62%), Space: 14 MB (8.47%) - LeetHub

--- 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.py
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.py
@@ -1,8 +1,15 @@
 class Solution:
     def reversePrefix(self, word: str, ch: str) -> str:
+        """
         if ch not in word:
             return word
         else:
             idx = word.index(ch)
             ret = word[:idx + 1]
+            return word.replace(ret, ret[::-1])
+        """
+        if ch not in word:
+            return word
+        else:
+            ret = word[:word.index(ch) + 1]
             return word.replace(ret, ret[::-1])
