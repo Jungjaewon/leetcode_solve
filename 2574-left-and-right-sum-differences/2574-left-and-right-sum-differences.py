64f91a1a0dacecac47ac19110ec72f9accab0b2f
정재원 <Woodcook486@naver.com>
2023-03-01 13:59:14 +0900

    Time: 62 ms (98.86%), Space: 14.3 MB (15.41%) - LeetHub

--- 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.py
+++ 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.py
@@ -1,15 +1,9 @@
 class Solution:
     def leftRigthDifference(self, nums: List[int]) -> List[int]:
-        
         from itertools import accumulate
-        
         left_arr = [0] + nums[0:len(nums) - 1]
         right_arr = nums[1:len(nums)] + [0]
-        
         left_arr = list(accumulate(left_arr))
-        right_arr = list(accumulate(right_arr[::-1]))[::-1]
-        
-        ans_arr = list()
-        
+        right_arr = list(accumulate(right_arr[::-1]))[::-1]        
         return [abs(a - b) for a,b in zip(left_arr, right_arr)]
         
