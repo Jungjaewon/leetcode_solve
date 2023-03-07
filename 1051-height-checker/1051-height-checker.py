049714b9f91817255b28d47ceb2f891173c436f1
정재원 <Woodcook486@naver.com>
2023-03-07 18:49:14 +0900

    Time: 30 ms (95.41%), Space: 13.9 MB (45.94%) - LeetHub

---
+++ 1051-height-checker/1051-height-checker.py
@@ -0,0 +1,4 @@
+class Solution:
+    def heightChecker(self, heights: List[int]) -> int:
+        print(sorted(heights))
+        return sum([1 if x != y else 0 for x,y in zip(heights, sorted(heights))])
