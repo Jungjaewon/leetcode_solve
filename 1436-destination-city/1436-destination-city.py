62850226b968560a73e64ed6ddbca98b0b61f510
정재원 <Woodcook486@naver.com>
2023-03-09 20:17:03 +0900

    Time: 51 ms (84.18%), Space: 13.9 MB (31.49%) - LeetHub

--- 1436-destination-city/1436-destination-city.py
+++ 1436-destination-city/1436-destination-city.py
@@ -1,9 +1,16 @@
 class Solution:
     def destCity(self, paths: List[List[str]]) -> str:
+        """        
         ans_dict = {a : b for a,b in paths}
         city_set = {b for a,b in paths}
         for city in city_set:
             if city not in ans_dict:
                 return city
+        """
+        ans_dict = {a : b for a,b in paths}
+        for A, B in ans_dict.items():
+            if B not in ans_dict:
+                return B
+        
         
         
