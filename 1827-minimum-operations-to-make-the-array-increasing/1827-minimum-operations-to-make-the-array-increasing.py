77743d30c567444d0763f56c24073a2ad91f3593
정재원 <Woodcook486@naver.com>
2023-03-18 17:59:43 +0900

    Time: 132 ms (64.02%), Space: 14.7 MB (40.68%) - LeetHub

--- 1827-minimum-operations-to-make-the-array-increasing/1827-minimum-operations-to-make-the-array-increasing.py
+++ 1827-minimum-operations-to-make-the-array-increasing/1827-minimum-operations-to-make-the-array-increasing.py
@@ -1,9 +1,18 @@
 class Solution:
     def minOperations(self, nums: List[int]) -> int:
+        """
         ans = 0
         for i in range(len(nums) - 1):
             if nums[i] >= nums[i + 1]:
                 gap = abs(nums[i] - nums[i + 1]) + 1
                 ans += gap
                 nums[i + 1] = nums[i + 1] + gap
+        return ans
+        """
+        ans = 0
+        for i in range(len(nums) - 1):
+            if nums[i] >= nums[i + 1]:
+                gap = nums[i] - nums[i + 1] + 1
+                ans += gap
+                nums[i + 1] += gap
         return ans
