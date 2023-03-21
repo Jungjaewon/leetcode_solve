58dc4fcb7c513c579d4c570c04a871890abd5dda
정재원 <Woodcook486@naver.com>
2023-03-21 19:32:10 +0900

    Time: 38 ms (42.65%), Space: 13.8 MB (43.55%) - LeetHub

--- 2595-number-of-even-and-odd-bits/2595-number-of-even-and-odd-bits.py
+++ 2595-number-of-even-and-odd-bits/2595-number-of-even-and-odd-bits.py
@@ -1,10 +1,20 @@
 class Solution:
     def evenOddBit(self, n: int) -> List[int]:
+        """
         ans_l = [0,0]
         for i in range(32):
             if i & 1 == 0 and n & 1 << i:
                 ans_l[0] += 1
             elif i & 1 != 0 and n & 1 << i:
                 ans_l[1] += 1
+        return ans_l
+        """
+        ans_l = [0,0]
+        for i in range(32):
+            if n & 1 << i:
+                if i & 1 == 0:
+                    ans_l[0] += 1
+                else:
+                    ans_l[1] += 1
         return ans_l
             
