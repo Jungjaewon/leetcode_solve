3e39e4fed5079dfbb26f10fc05ed49e726b9f712
정재원 <Woodcook486@naver.com>
2023-03-22 22:19:02 +0900

    Time: 69 ms (62.84%), Space: 14.2 MB (40.77%) - LeetHub

--- 2586-count-the-number-of-vowel-strings-in-range/2586-count-the-number-of-vowel-strings-in-range.py
+++ 2586-count-the-number-of-vowel-strings-in-range/2586-count-the-number-of-vowel-strings-in-range.py
@@ -1,4 +1,5 @@
 class Solution:
     def vowelStrings(self, words: List[str], left: int, right: int) -> int:
-        vowel = ['a', 'e', 'i', 'o', 'u']
-        return sum([ 1 for word in words[left:right + 1] if word[0] in vowel and word[-1] in vowel])
+        #vowel = ['a', 'e', 'i', 'o', 'u']
+        #return sum([ 1 for word in words[left:right + 1] if word[0] in vowel and word[-1] in vowel])
+        return sum([ 1 for word in words[left:right + 1] if word[0] in ['a', 'e', 'i', 'o', 'u'] and word[-1] in ['a', 'e', 'i', 'o', 'u']])
