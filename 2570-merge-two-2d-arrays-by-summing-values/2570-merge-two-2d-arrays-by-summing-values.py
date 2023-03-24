a841430a7ae68ccdd92c2804382ac1aa84301588
정재원 <Woodcook486@naver.com>
2023-03-25 07:41:48 +0900

    Time: 65 ms (63.12%), Space: 14.2 MB (31.43%) - LeetHub

--- 2570-merge-two-2d-arrays-by-summing-values/2570-merge-two-2d-arrays-by-summing-values.py
+++ 2570-merge-two-2d-arrays-by-summing-values/2570-merge-two-2d-arrays-by-summing-values.py
@@ -1,6 +1,14 @@
 class Solution:
     def mergeArrays(self, nums1: List[List[int]], nums2: List[List[int]]) -> List[List[int]]:
+        """
         ans_l, a_dict = list(), defaultdict(int)
+        for idx, n in nums1:
+            a_dict[idx] += n
+        for idx, n in nums2:
+            a_dict[idx] +=n
+        return [[idx, a_dict[idx]] for idx in sorted(a_dict.keys())]
+        """
+        a_dict = defaultdict(int)
         for idx, n in nums1:
             a_dict[idx] += n
         for idx, n in nums2:
