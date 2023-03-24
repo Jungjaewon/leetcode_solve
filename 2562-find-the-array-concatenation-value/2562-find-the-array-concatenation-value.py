6c330ca3c8fe8e42ca9913ebcffd598a4c296890
정재원 <Woodcook486@naver.com>
2023-03-24 17:41:02 +0900

    Time: 83 ms (5.51%), Space: 14.2 MB (6.19%) - LeetHub

--- 2562-find-the-array-concatenation-value/2562-find-the-array-concatenation-value.py
+++ 2562-find-the-array-concatenation-value/2562-find-the-array-concatenation-value.py
@@ -1,5 +1,6 @@
 class Solution:
     def findTheArrayConcVal(self, nums: List[int]) -> int:
+        """
         ans = 0
         while len(nums):
             if len(nums) == 1:
@@ -9,4 +10,15 @@ class Solution:
                 ans += int(str(nums[0]) + str(nums[-1]))
                 del nums[0]
                 del nums[-1]
+        return ans
+        """
+        ans = 0
+        while len(nums):
+            if len(nums) == 1:
+                ans += nums[0]
+                break
+            else:
+                ans += int(str(nums[0]) + str(nums[-1]))
+                nums = nums[1:]
+                nums = nums[:-1]
         return ans
