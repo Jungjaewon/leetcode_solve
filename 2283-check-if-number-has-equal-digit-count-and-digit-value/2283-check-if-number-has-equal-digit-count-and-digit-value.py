0bb05379976f8639a13e7921e8cfcf4cf13ae74a
정재원 <Woodcook486@naver.com>
2023-03-29 23:49:49 +0900

    Time: 37 ms (30.24%), Space: 13.8 MB (93.20%) - LeetHub

--- 2283-check-if-number-has-equal-digit-count-and-digit-value/2283-check-if-number-has-equal-digit-count-and-digit-value.py
+++ 2283-check-if-number-has-equal-digit-count-and-digit-value/2283-check-if-number-has-equal-digit-count-and-digit-value.py
@@ -3,7 +3,6 @@ class Solution:
         a_dict = defaultdict(int)
         for c in num:
             a_dict[int(c)] += 1
-        print(a_dict)
         for idx, c in enumerate(num):
             #print(f'c : {c}, {a_dict[idx]}, idx : {idx}')
             if int(c) != a_dict[idx]:
