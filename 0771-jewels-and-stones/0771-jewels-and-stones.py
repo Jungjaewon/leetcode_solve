7b7416bdf8564ae0e45a251c0f2d7c0407fe35d5
정재원 <Woodcook486@naver.com>
2023-04-02 09:21:28 +0900

    Time: 30 ms (76.63%), Space: 13.8 MB (94.24%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.py
+++ 0771-jewels-and-stones/0771-jewels-and-stones.py
@@ -1,7 +1,13 @@
 class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        """
         ans = 0
         for c in jewels:
             ans += len([ 1 for s in stones if c == s])
+        return ans
+        """
+        ans = 0
+        for c in jewels:
+            ans += sum([ 1 for s in stones if c == s])
         return ans
             
