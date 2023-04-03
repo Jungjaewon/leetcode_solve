fe2ca7a2d55aa24da3b6dd0e197173b057358e5d
정재원 <Woodcook486@naver.com>
2023-04-03 20:38:27 +0900

    Time: 72 ms (63.87%), Space: 14.4 MB (76.71%) - LeetHub

--- 2085-count-common-words-with-one-occurrence/2085-count-common-words-with-one-occurrence.py
+++ 2085-count-common-words-with-one-occurrence/2085-count-common-words-with-one-occurrence.py
@@ -1,5 +1,6 @@
 class Solution:
     def countWords(self, words1: List[str], words2: List[str]) -> int:
+        """
         a_dict, b_dict, ans = defaultdict(int), defaultdict(int), 0
         for w in words1:
             a_dict[w] += 1
@@ -9,4 +10,14 @@ class Solution:
             if a_dict[word] == 1:
                 if word in b_dict and b_dict[word] == 1:
                     ans += 1
+        return ans
+        """
+        a_dict, b_dict, ans = defaultdict(int), defaultdict(int), 0
+        for w in words1:
+            a_dict[w] += 1
+        for w in words2:
+            b_dict[w] += 1
+        for word in a_dict:
+            if a_dict[word] == 1 and  word in b_dict and b_dict[word] == 1:
+                ans += 1
         return ans
