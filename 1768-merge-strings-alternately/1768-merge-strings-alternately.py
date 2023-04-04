123f71b32d94af2629f2358b4a943529bc644cbb
정재원 <Woodcook486@naver.com>
2023-04-04 19:21:28 +0900

    Time: 36 ms (33.58%), Space: 14 MB (10.04%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.py
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -26,6 +26,7 @@ class Solution:
             ans.append(b)
         return ''.join(ans)
         """
+        """
         word1_list, word2_list, ans = list(word1), list(word2), list()
         for _ in range(abs(len(word1_list) - len(word2_list))):
             if len(word1_list) > len(word2_list):
@@ -36,3 +37,18 @@ class Solution:
             ans.append(a)
             ans.append(b)
         return ''.join(ans)
+        """
+        word1_list, word2_list, ans = list(word1), list(word2), list()
+        if len(word1_list) >= len(word2_list):
+            ret = word1_list[len(word2_list):]
+            word1_list = word1_list[:len(word2_list)]
+        else:
+            ret = word2_list[len(word1_list):]
+            word2_list = word2_list[:len(word1_list)]
+        for a,b in zip(word1_list, word2_list):
+            ans.append(a + b)
+        print(ans)
+        print(ret)
+        return ''.join(ans) + (''.join(ret) if len(ret) else '')
+        
+        
