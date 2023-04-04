fe313d8f547594a4ca56c5f12e95539e820f77e3
정재원 <Woodcook486@naver.com>
2023-04-04 18:58:50 +0900

    Time: 38 ms (18.52%), Space: 13.8 MB (56.72%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.py
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -13,9 +13,21 @@ class Solution:
             ans.append(b)
         return ''.join(ans).replace('*', '')
         """
+        """
         word1_list, word2_list, ans = list(word1), list(word2), list()
         gap = abs(len(word1_list) - len(word2_list))
         for _ in range(gap):
+            if len(word1_list) > len(word2_list):
+                word2_list.append('')
+            else:
+                word1_list.append('')
+        for a,b in zip (word1_list, word2_list):
+            ans.append(a)
+            ans.append(b)
+        return ''.join(ans)
+        """
+        word1_list, word2_list, ans = list(word1), list(word2), list()
+        for _ in range(abs(len(word1_list) - len(word2_list))):
             if len(word1_list) > len(word2_list):
                 word2_list.append('')
             else:
