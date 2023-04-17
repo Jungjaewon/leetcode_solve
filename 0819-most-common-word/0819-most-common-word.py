d3cec5e0b156c7afbed6a77426eee8e205fea8c4
정재원 <Woodcook486@naver.com>
2023-04-17 21:24:57 +0900

    Time: 30 ms (94.32%), Space: 13.9 MB (66.81%) - LeetHub

---
+++ 0819-most-common-word/0819-most-common-word.py
@@ -0,0 +1,17 @@
+class Solution:
+    def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
+        for c in "!?',;.":
+            paragraph = paragraph.replace(c, " ")
+        para_dict = dict(Counter(paragraph.lower().split(' ')))
+        print(para_dict)
+        if '' in para_dict: 
+            del para_dict['']
+        for b in banned:
+            if b in para_dict:
+                del para_dict[b]
+        para_list = [[key, para_dict[key]] for key in para_dict]
+        para_list.sort(key=lambda x : -x[-1])
+        return para_list[0][0]
+        
+        
+        
