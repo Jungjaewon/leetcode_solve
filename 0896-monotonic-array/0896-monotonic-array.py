0860bcf450b99153bf57fae8cb51faac13c97bdf
정재원 <Woodcook486@naver.com>
2023-04-18 22:36:32 +0900

    Time: 1045 ms (29.07%), Space: 28.8 MB (9.99%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.py
+++ 0896-monotonic-array/0896-monotonic-array.py
@@ -1,5 +1,6 @@
 class Solution:
     def isMonotonic(self, nums: List[int]) -> bool:
+        """
         if len(nums) == 1:
             return True
         elif len(nums) == 2:
@@ -20,7 +21,22 @@ class Solution:
                     result_2.append(True)
                 else:
                     result_2.append(False)
-            #print(all(result_1), result_1)
-            #print(all(result_2), result_2)
             return all(result_1) or all(result_2)
+        """
+        result_1, result_2 = list(), list()
+        temp = nums[0]
+        for n in nums[1:]:
+            if temp <= n:
+                temp = n
+                result_1.append(True)
+            else:
+                result_1.append(False)
+        temp = nums[0]
+        for n in nums[1:]:
+            if temp >= n:
+                temp = n
+                result_2.append(True)
+            else:
+                result_2.append(False)
+        return all(result_1) or all(result_2)
             
