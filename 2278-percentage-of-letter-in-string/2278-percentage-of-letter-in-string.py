e7a1fc0c8bb517459120cb4d7b87595336aeb828
정재원 <Woodcook486@naver.com>
2023-04-22 17:07:54 +0900

    Time: 27 ms (86.65%), Space: 13.8 MB (41.08%) - LeetHub

--- 2278-percentage-of-letter-in-string/2278-percentage-of-letter-in-string.py
+++ 2278-percentage-of-letter-in-string/2278-percentage-of-letter-in-string.py
@@ -1,4 +1,5 @@
 class Solution:
     def percentageLetter(self, s: str, letter: str) -> int:
         #return int((sum([1 for c in s if c == letter]) / float(len(s))) * 100)
-        return int(Counter(s)[letter] / float(len(s)) * 100)
+        #return int(Counter(s)[letter] / float(len(s)) * 100)
+        return int(s.count(letter) / float(len(s)) * 100)
