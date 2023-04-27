be5dc14f509f298b4d9633e384fb90766cc3641f
정재원 <Woodcook486@naver.com>
2023-04-27 18:16:28 +0900

    Time: 87 ms (5.65%), Space: 16.6 MB (35.14%) - LeetHub

--- 1629-slowest-key/1629-slowest-key.py
+++ 1629-slowest-key/1629-slowest-key.py
@@ -1,5 +1,6 @@
 class Solution:
     def slowestKey(self, releaseTimes: List[int], keysPressed: str) -> str:
+        """
         temp_l = []
         for idx, t in enumerate(releaseTimes):
             if idx == 0:
@@ -8,6 +9,13 @@ class Solution:
                 #print(f'{t} - {releaseTimes[idx - 1]}')
                 temp_l.append(abs(releaseTimes[idx - 1] - t))
         #print(temp_l)
+        ans_l = list(zip(temp_l, keysPressed))
+        ans_l.sort(key=lambda x: (x[0], x[1]))
+        return ans_l[-1][1]
+        """
+        temp_l = []
+        for idx, t in enumerate(releaseTimes):
+            temp_l.append(t if idx == 0 else abs(releaseTimes[idx - 1] - t))
         ans_l = list(zip(temp_l, keysPressed))
         ans_l.sort(key=lambda x: (x[0], x[1]))
         return ans_l[-1][1]
