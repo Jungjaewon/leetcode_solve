0358a0987ca49a8e0bb3e345d2fb0545b64ef95c
정재원 <Woodcook486@naver.com>
2023-04-30 10:31:29 +0900

    Time: 122 ms (7.22%), Space: 16.5 MB (5.20%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.py
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.py
@@ -8,9 +8,12 @@ class Solution:
         else:
             return sorted(arr, key=lambda x : (func(x), x))
         """
+        """
         def func(n):
             return sum([1 for i in range(32) if n & 1 << i])
         if len(set([func(n) for n in arr])) == 1:
             return sorted(arr)
         else:
             return sorted(arr, key=lambda x : (func(x), x))
+        """
+        return sorted(arr, key=lambda x : (sum([1 for i in range(32) if x & 1 << i]), x))
