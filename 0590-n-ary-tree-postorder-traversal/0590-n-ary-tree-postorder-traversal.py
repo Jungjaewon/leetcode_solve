e9be7383b2cca9dd7e0eeaa28edd257ae715ad8d
정재원 <Woodcook486@naver.com>
2023-05-08 22:15:51 +0900

    Time: 75 ms (5.17%), Space: 18.7 MB (6.86%) - LeetHub

--- 0590-n-ary-tree-postorder-traversal/0590-n-ary-tree-postorder-traversal.py
+++ 0590-n-ary-tree-postorder-traversal/0590-n-ary-tree-postorder-traversal.py
@@ -8,11 +8,27 @@ class Node:
 
 class Solution:
     def postorder(self, root: 'Node') -> List[int]:
+        """
         if root is None:
             return []
         else:
             sub_list = list()
             for ch in root.children:
                 sub_list += self.postorder(ch)
+            sub_list.append(root.val)
+            return sub_list
+        """
+        if root is None:
+            return []
+        else:
+            sub_list = list()
+            for ch in root.children:
+                res = self.postorder(ch)
+                if len(res):
+                    if isinstance(res, list):
+                        for r in res:
+                            sub_list.append(r)
+                    else:
+                        sub_list.append(res)
             sub_list.append(root.val)
             return sub_list
