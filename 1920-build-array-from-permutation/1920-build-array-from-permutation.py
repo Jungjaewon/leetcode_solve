07a4c27a6ff6cb03865a8c31566be5a99bc94751
정재원 <Woodcook486@naver.com>
2023-05-16 17:57:26 +0900

    Time: 137 ms (9.49%), Space: 16.6 MB (15.28%) - LeetHub

--- 1920-build-array-from-permutation/1920-build-array-from-permutation.py
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.py
@@ -1,6 +1,12 @@
 class Solution:
     def buildArray(self, nums: List[int]) -> List[int]:
+        """
         ans, cnt = list(), 0
+        for i in range(len(nums)):
+            ans.append(nums[nums[i]])
+        return ans
+        """
+        ans = list()
         for i in range(len(nums)):
             ans.append(nums[nums[i]])
         return ans
