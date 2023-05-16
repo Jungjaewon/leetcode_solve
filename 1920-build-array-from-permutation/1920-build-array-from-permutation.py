db15c6f30fe4845b98e66a5197a66073dc6a1610
정재원 <Woodcook486@naver.com>
2023-05-16 17:57:01 +0900

    Time: 122 ms (71.10%), Space: 16.6 MB (15.28%) - LeetHub

---
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.py
@@ -0,0 +1,6 @@
+class Solution:
+    def buildArray(self, nums: List[int]) -> List[int]:
+        ans, cnt = list(), 0
+        for i in range(len(nums)):
+            ans.append(nums[nums[i]])
+        return ans
