230dd4b77f58206155a9c77e458b1f52db2c4059
정재원 <Woodcook486@naver.com>
2023-05-20 14:58:36 +0900

    Time: 100 ms (28.14%), Space: 16.2 MB (39.37%) - LeetHub

--- 2652-sum-multiples/2652-sum-multiples.py
+++ 2652-sum-multiples/2652-sum-multiples.py
@@ -1,7 +1,10 @@
 class Solution:
     def sumOfMultiples(self, n: int) -> int:
+        """
         ans = 0
         for num in range(1, n + 1):
             if num % 3 == 0 or num % 5 == 0 or num % 7 == 0:
                 ans += num
         return ans
+        """
+        return sum([num for num in range(1, n + 1) if num % 3 == 0 or num % 5 == 0 or num % 7 == 0])
