2b17a3889791bb7b958c6e7d45ec18742a3da7bb
정재원 <Woodcook486@naver.com>
2023-05-30 18:44:26 +0900

    Time: 228 ms (24.86%), Space: 17.9 MB (8.07%) - LeetHub

--- 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.py
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.py
@@ -1,10 +1,14 @@
 class Solution:
     def repeatedNTimes(self, nums: List[int]) -> int:
         a_dict = dict(Counter(nums))
+        return [ key for key, repeated in a_dict.items() if repeated == len(a_dict) - 1][0]
+        """
+        a_dict = dict(Counter(nums))
         unique_n = len(a_dict)
         for key, repeated in a_dict.items():
             if repeated == unique_n - 1:
                 return key
+        """
         """
         unique_n = len(set(nums)) 
         for key, repeated in dict(Counter(nums)).items():
