10badb96bc70581fb479dddef5ab4035f49925ad
정재원 <Woodcook486@naver.com>
2023-06-01 18:56:24 +0900

    Time: 56 ms (5.10%), Space: 16.4 MB (6.29%) - LeetHub

--- 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.py
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.py
@@ -7,8 +7,15 @@ class Solution:
             if t_dict[c] == 2:
                 return c
         """
+        """
         a_list = [0] * 26
         for c in s:
             a_list[ord(c) - ord('a')] += 1
             if a_list[ord(c) - ord('a')] == 2:
                 return c
+        """
+        a_set = set()
+        for c in s:
+            if c in a_set:
+                return c
+            a_set.add(c)
