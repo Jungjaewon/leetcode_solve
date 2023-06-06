460be8ca2267839cbde8dc012b158f0709092f61
정재원 <Woodcook486@naver.com>
2023-06-06 09:42:32 +0900

    Time: 98 ms (88.35%), Space: 17.8 MB (5.22%) - LeetHub

--- 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.py
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.py
@@ -1,11 +1,14 @@
 class Solution:
     def minDeletionSize(self, strs: List[str]) -> int:
+        """
         ans = 0
         col_list = list(zip(*strs))
         for col in col_list:
             if list(col) != sorted(col):
                 ans += 1
         return ans
+        """
+        return sum([1 for col in list(zip(*strs)) if list(col) != sorted(col)])
         
         
         
