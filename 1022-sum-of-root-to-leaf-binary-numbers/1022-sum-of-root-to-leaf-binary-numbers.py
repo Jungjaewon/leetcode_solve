7756c238c97130ec16521e44865fe093af6139fe
정재원 <Woodcook486@naver.com>
2023-06-11 13:35:24 +0900

    Time: 48 ms (65.47%), Space: 16.8 MB (16.67%) - LeetHub

--- 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.py
+++ 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.py
@@ -21,6 +21,7 @@ class Solution:
         func(root, "")
         return sum([int(x, 2) for x in ans_list])
         """
+        """
         ans_list = list()
         def func(root: Optional[TreeNode], s : str):
             if root is None:
@@ -34,3 +35,18 @@ class Solution:
                     func(root.right, s)
         func(root, "")
         return sum([int(x, 2) for x in ans_list])
+        """
+        ans = 0 
+        def func(root: Optional[TreeNode], n : int):
+            nonlocal ans
+            if root is None:
+                return
+            else:
+                n = n << 1 | root.val
+                if root.left is None and root.right is None:
+                    ans += n
+                else:
+                    func(root.left, n)
+                    func(root.right, n)
+        func(root, 0)
+        return ans
