e2cdc4cb9f330738769a6d96b020c7afcc32e2f4
정재원 <Woodcook486@naver.com>
2023-06-14 11:23:38 +0900

    Time: 771 ms (64.73%), Space: 49.3 MB (16.77%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -5,11 +5,28 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        """
         a_list = list()
         temp = head
         while temp:
             a_list.append(temp.val)
             temp = temp.next
+        num_a = len(a_list)
+        if num_a & 1: # odd
+            if a_list[:(num_a // 2)] == a_list[(num_a // 2) + 1:][::-1]:
+                return True
+            else:
+                return False
+        else: # even
+            if a_list[:num_a // 2] == a_list[num_a // 2:][::-1]:
+                return True
+            else:
+                return False
+        """
+        a_list = list()
+        while head:
+            a_list.append(head.val)
+            head = head.next
         num_a = len(a_list)
         if num_a & 1: # odd
             if a_list[:(num_a // 2)] == a_list[(num_a // 2) + 1:][::-1]:
