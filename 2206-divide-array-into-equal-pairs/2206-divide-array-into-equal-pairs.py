02d835011f58e36ebee6cae37e65165a0f2e90ef
정재원 <Woodcook486@naver.com>
2023-06-19 20:35:35 +0900

    Time: 103 ms (53.64%), Space: 16.5 MB (25.54%) - LeetHub

--- 2206-divide-array-into-equal-pairs/2206-divide-array-into-equal-pairs.py
+++ 2206-divide-array-into-equal-pairs/2206-divide-array-into-equal-pairs.py
@@ -1,7 +1,14 @@
 class Solution:
     def divideArray(self, nums: List[int]) -> bool:
+        """
         a_dict = dict(Counter(nums))
         for _, item in a_dict.items():
             if item % 2 != 0:
+                return False
+        return True
+        """
+        a_dict = dict(Counter(nums))
+        for _, item in a_dict.items():
+            if item & 1 != 0:
                 return False
         return True
