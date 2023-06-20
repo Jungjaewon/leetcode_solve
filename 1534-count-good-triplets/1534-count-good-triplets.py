5269a4cdf3746386a2e320919949fa211a7548d2
정재원 <Woodcook486@naver.com>
2023-06-20 17:17:58 +0900

    Time: 647 ms (48.16%), Space: 16.4 MB (15.26%) - LeetHub

--- 1534-count-good-triplets/1534-count-good-triplets.py
+++ 1534-count-good-triplets/1534-count-good-triplets.py
@@ -1,5 +1,6 @@
 class Solution:
     def countGoodTriplets(self, arr: List[int], a: int, b: int, c: int) -> int:
+        """
         ans = list()
         for i in range(len(arr)):
             for j in range(i + 1, len(arr)):
@@ -7,3 +8,11 @@ class Solution:
                     if abs(arr[i] - arr[j]) <= a and abs(arr[j] - arr[k]) <= b and abs(arr[i] - arr[k]) <= c:
                         ans.append((arr[i],arr[j],arr[k]))
         return len(ans)
+        """
+        ans = 0
+        for i in range(len(arr)):
+            for j in range(i + 1, len(arr)):
+                for k in range(j + 1, len(arr)):
+                    if abs(arr[i] - arr[j]) <= a and abs(arr[j] - arr[k]) <= b and abs(arr[i] - arr[k]) <= c:
+                        ans += 1
+        return ans
