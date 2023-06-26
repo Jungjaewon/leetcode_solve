71170869cc6de76207eef94060120a6595f9b9f8
정재원 <Woodcook486@naver.com>
2023-06-26 19:00:23 +0900

    Time: 44 ms (76.13%), Space: 16.4 MB (16.26%) - LeetHub

--- 2651-calculate-delayed-arrival-time/2651-calculate-delayed-arrival-time.py
+++ 2651-calculate-delayed-arrival-time/2651-calculate-delayed-arrival-time.py
@@ -1,3 +1,4 @@
 class Solution:
     def findDelayedArrivalTime(self, arrivalTime: int, delayedTime: int) -> int:
-        return (arrivalTime + delayedTime) % 24
+        #return (arrivalTime + delayedTime) % 24
+        return (arrivalTime + delayedTime) if (arrivalTime + delayedTime) < 24 else abs(24 - (arrivalTime + delayedTime)) 
