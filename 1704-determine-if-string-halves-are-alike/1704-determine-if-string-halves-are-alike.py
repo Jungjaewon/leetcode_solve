372b67aff0e104258614db0c12729f1ea106deac
정재원 <Woodcook486@naver.com>
2023-06-28 19:45:01 +0900

    Time: 41 ms (95.58%), Space: 16.1 MB (99.58%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.py
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.py
@@ -1,6 +1,11 @@
 class Solution:
     def halvesAreAlike(self, s: str) -> bool:
+        """
         a, b = s[:len(s) // 2], s[len(s) // 2:]
         a_sum = sum([a.count(c) for c in ['a','e','i','o','u', 'A', 'E', 'I', 'O', 'U']])
         b_sum = sum([b.count(c) for c in ['a','e','i','o','u', 'A', 'E', 'I', 'O', 'U']])
+        return a_sum == b_sum
+        """
+        a_sum = sum([s[:len(s) // 2].count(c) for c in ['a','e','i','o','u', 'A', 'E', 'I', 'O', 'U']])
+        b_sum = sum([s[len(s) // 2:].count(c) for c in ['a','e','i','o','u', 'A', 'E', 'I', 'O', 'U']])
         return a_sum == b_sum
