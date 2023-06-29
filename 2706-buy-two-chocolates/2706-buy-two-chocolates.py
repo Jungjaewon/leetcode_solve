cd287b63250a40f2580835417b891671f1c8d606
정재원 <Woodcook486@naver.com>
2023-06-29 18:24:02 +0900

    Time: 80 ms (45.96%), Space: 16.4 MB (6.23%) - LeetHub

--- 2706-buy-two-chocolates/2706-buy-two-chocolates.py
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.py
@@ -1,4 +1,5 @@
 class Solution:
     def buyChoco(self, prices: List[int], money: int) -> int:
-        two_ch = sum(sorted(prices)[:2])
-        return money if two_ch > money else money - two_ch
+        #two_ch = sum(sorted(prices)[:2])
+        #return money if two_ch > money else money - two_ch
+        return money if sum(sorted(prices)[:2]) > money else money - sum(sorted(prices)[:2])
