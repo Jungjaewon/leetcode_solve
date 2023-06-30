acc411090b41eec20b8d55d8b1cf82788d932259
정재원 <Woodcook486@naver.com>
2023-06-30 23:06:12 +0900

    Time: 68 ms (51.56%), Space: 16.6 MB (11.65%) - LeetHub

--- 1002-find-common-characters/1002-find-common-characters.py
+++ 1002-find-common-characters/1002-find-common-characters.py
@@ -18,6 +18,7 @@ class Solution:
                     ans.append(c)
         return ans
         """
+        """
         ans, c_words = list(), [Counter(word) for word in words]
         for c in c_words[0]:
             num, flag = c_words[0][c], True
@@ -31,4 +32,13 @@ class Solution:
             if flag:
                 for _ in range(num):
                     ans.append(c)
+        return ans
+        """
+        ans, c_words = list(), [dict(Counter(word)) for word in words]
+        c_set = set([ x for c_dict in c_words for x in list(c_dict.keys())])
+        for c in c_set:
+            if all([True if c in c_d else False for c_d in c_words]):
+                n = min([c_d[c] if c in c_d else 10e4 for c_d in c_words])
+                for _ in range(n):
+                    ans.append(c)  
         return ans
