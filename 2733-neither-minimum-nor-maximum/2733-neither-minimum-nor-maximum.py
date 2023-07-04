69a334203110e86fc6a6619dc4be9b5b46aa03cf
정재원 <Woodcook486@naver.com>
2023-07-04 22:08:47 +0900

    Time: 392 ms (51.17%), Space: 16.4 MB (48.10%) - LeetHub

--- 2733-neither-minimum-nor-maximum/2733-neither-minimum-nor-maximum.py
+++ 2733-neither-minimum-nor-maximum/2733-neither-minimum-nor-maximum.py
@@ -7,8 +7,15 @@ class Solution:
                 return d
         return -1
         """
+        """
         if len(nums) <= 2:
             return -1
         else:
             nums = sorted(nums)
+            return nums[1]
+        """
+        nums = sorted(nums)
+        if len(nums) <= 2:
+            return -1
+        else:
             return nums[1]
