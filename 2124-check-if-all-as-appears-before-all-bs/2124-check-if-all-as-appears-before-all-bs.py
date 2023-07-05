d4d147ce38ea20b11938ce1eeb37bd1e3932f781
정재원 <Woodcook486@naver.com>
2023-07-05 19:00:40 +0900

    Time: 41 ms (82.76%), Space: 16.3 MB (25.79%) - LeetHub

---
+++ 2124-check-if-all-as-appears-before-all-bs/2124-check-if-all-as-appears-before-all-bs.py
@@ -0,0 +1,14 @@
+class Solution:
+    def checkString(self, s: str) -> bool:
+        c_dict = dict(Counter(s))
+        if 'a' not in c_dict:
+            return True
+        
+        for c in s:
+            if c == 'a':
+                c_dict[c] -= 1
+            elif c == 'b' and c_dict['a'] == 0:
+                c_dict[c] -= 1
+            else:
+                return False
+        return True
