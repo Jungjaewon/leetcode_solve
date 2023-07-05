93d9ad52ca9542f0d2bca4c276b4f18d6ab0dd39
정재원 <Woodcook486@naver.com>
2023-07-05 21:44:39 +0900

    Time: 45 ms (60.66%), Space: 16.2 MB (93.95%) - LeetHub

--- 2124-check-if-all-as-appears-before-all-bs/2124-check-if-all-as-appears-before-all-bs.py
+++ 2124-check-if-all-as-appears-before-all-bs/2124-check-if-all-as-appears-before-all-bs.py
@@ -1,9 +1,10 @@
 class Solution:
     def checkString(self, s: str) -> bool:
+        """
         c_dict = dict(Counter(s))
         if 'a' not in c_dict:
             return True
-        
+         
         for c in s:
             if c == 'a':
                 c_dict[c] -= 1
@@ -12,3 +13,18 @@ class Solution:
             else:
                 return False
         return True
+        """
+        if 'a' not in s:
+            return True
+        
+        c_dict = dict(Counter(s))
+         
+        for c in s:
+            if c == 'a':
+                c_dict[c] -= 1
+            elif c == 'b' and c_dict['a'] == 0:
+                c_dict[c] -= 1
+            else:
+                return False
+        return True
+        
