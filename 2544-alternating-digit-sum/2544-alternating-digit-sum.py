9f17922fb902de968fb41d9c25eb95c298757920
정재원 <Woodcook486@naver.com>
2023-07-08 21:49:34 +0900

    Time: 55 ms (6.60%), Space: 16.3 MB (64.08%) - LeetHub

--- 2544-alternating-digit-sum/2544-alternating-digit-sum.py
+++ 2544-alternating-digit-sum/2544-alternating-digit-sum.py
@@ -1,3 +1,10 @@
 class Solution:
     def alternateDigitSum(self, n: int) -> int:
-        return sum([ int(x) if idx & 1 == 0 else -int(x) for idx, x in enumerate(str(n))])
+        #return sum([ int(x) if idx & 1 == 0 else -int(x) for idx, x in enumerate(str(n))])
+        ans = 0 
+        for idx, x in enumerate(str(n)):
+            if idx & 1 == 0:
+                ans += int(x)
+            else:
+                ans -= int(x)
+        return ans
