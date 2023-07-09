af9764f3974773e3579531325ff32e73f5b1e86a
정재원 <Woodcook486@naver.com>
2023-07-09 22:41:04 +0900

    Time: 62 ms (67.33%), Space: 16.3 MB (68.95%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.py
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.py
@@ -1,5 +1,6 @@
 class Solution:
     def pivotInteger(self, n: int) -> int:
+        """
         num_l = list(range(1, n + 1))
         if len(num_l) == 1:
             return n
@@ -8,5 +9,20 @@ class Solution:
                 if sum(num_l[:idx + 1]) == sum(num_l[idx:]):
                     return num_l[idx]
             return -1
+        """
+        num_l = list(range(1, n + 1))
+        if len(num_l) == 1:
+            return n
+        else:
+            a,b, cnt = sum(num_l), 0, n
+            while cnt > 0:
+                if a == b +  cnt:
+                    return cnt
+                else:
+                    #print(a,b, cnt)
+                    a, b = a - cnt, b + cnt
+                    cnt -= 1
+            return -1
+        
                 
                 
