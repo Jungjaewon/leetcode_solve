49e2ff40ce1bca151e2f2b01b0622040f287576f
정재원 <Woodcook486@naver.com>
2023-07-11 18:41:21 +0900

    Time: 147 ms (39.97%), Space: 17.2 MB (33.94%) - LeetHub

--- 2363-merge-similar-items/2363-merge-similar-items.py
+++ 2363-merge-similar-items/2363-merge-similar-items.py
@@ -1,6 +1,14 @@
 class Solution:
     def mergeSimilarItems(self, items1: List[List[int]], items2: List[List[int]]) -> List[List[int]]:
+        """
         ans_dict = defaultdict(int)
         for n,w in items1 + items2:
+            ans_dict[n] += w
+        return [[key, ans_dict[key]] for key in sorted(ans_dict)]
+        """
+        ans_dict = defaultdict(int)
+        for n,w in items1:
+            ans_dict[n] += w
+        for n,w in items2:
             ans_dict[n] += w
         return [[key, ans_dict[key]] for key in sorted(ans_dict)]
