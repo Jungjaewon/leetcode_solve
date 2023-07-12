5f76e9b4e569f787b1ae22d06bd84395e3e31e5e
정재원 <Woodcook486@naver.com>
2023-07-12 18:29:08 +0900

    Time: 60 ms (48.16%), Space: 16.3 MB (77.66%) - LeetHub

--- 2399-check-distances-between-same-letters/2399-check-distances-between-same-letters.py
+++ 2399-check-distances-between-same-letters/2399-check-distances-between-same-letters.py
@@ -1,5 +1,6 @@
 class Solution:
     def checkDistances(self, s: str, distance: List[int]) -> bool:
+        """
         data_dict = defaultdict(list)
         for idx, c in enumerate(s):
             data_dict[c].append(idx)
@@ -8,4 +9,13 @@ class Solution:
             d = idx_list[-1] - idx_list[0] - 1
             if distance[ord(c) - ord('a')] != d:
                 return False
+        return True
+        """
+        data_dict = dict()
+        for idx, c in enumerate(s):
+            if c not in data_dict:
+                data_dict[c] = idx
+            else:
+                if idx - data_dict[c] - 1 != distance[ord(c) - ord('a')]:
+                    return False
         return True
