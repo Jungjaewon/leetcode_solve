3463da8234e16ae3fa52c924cb20c4bb636564ba
정재원 <Woodcook486@naver.com>
2023-07-15 13:14:04 +0900

    Time: 853 ms (12.50%), Space: 16.2 MB (72.94%) - LeetHub

--- 2475-number-of-unequal-triplets-in-array/2475-number-of-unequal-triplets-in-array.py
+++ 2475-number-of-unequal-triplets-in-array/2475-number-of-unequal-triplets-in-array.py
@@ -9,12 +9,27 @@ class Solution:
                         ans += 1
         return ans
         """
+        """
         ans = 0
         for i in range(len(nums)):
             for j in range(i + 1, len(nums)):
                 if nums[i] == nums[j]:
                     continue
                 for k in range(j + 1, len(nums)):
+                    if nums[i] != nums[j] and nums[j] != nums[k] and nums[i] != nums[k]:
+                        ans += 1
+        return ans
+        """
+        ans = 0
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] == nums[j]:
+                    continue
+                for k in range(j + 1, len(nums)):
+                    
+                    if nums[j] == nums[k]:
+                        continue
+                    
                     if nums[i] != nums[j] and nums[j] != nums[k] and nums[i] != nums[k]:
                         ans += 1
         return ans
