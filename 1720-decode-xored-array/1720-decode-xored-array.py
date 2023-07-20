b44cb49e72ea0276753da5776967e0c2d5addb11
정재원 <Woodcook486@naver.com>
2023-07-20 22:33:30 +0900

    Time: 233 ms (46.06%), Space: 18.3 MB (34.80%) - LeetHub

--- 1720-decode-xored-array/1720-decode-xored-array.py
+++ 1720-decode-xored-array/1720-decode-xored-array.py
@@ -1,9 +1,16 @@
 class Solution:
     def decode(self, encoded: List[int], first: int) -> List[int]:
+        """
         ans, temp = [first], first
         for e in encoded:
             result = e ^ temp
             ans.append(result)
             temp = result
         #print(ans)
+        return ans
+        """
+        ans = [first]
+        for e in encoded:
+            result = e ^ ans[-1]
+            ans.append(result)
         return ans
