30a741a02c421bcdfd3842a2995b2c59f1eb2b2e
정재원 <Woodcook486@naver.com>
2023-07-22 11:45:07 +0900

    Time: 60 ms (79.23%), Space: 16.3 MB (44.34%) - LeetHub

--- 2744-find-maximum-number-of-string-pairs/2744-find-maximum-number-of-string-pairs.py
+++ 2744-find-maximum-number-of-string-pairs/2744-find-maximum-number-of-string-pairs.py
@@ -8,5 +8,15 @@ class Solution:
                     ans += 1
         return ans
         """
-        return sum([1 for i in range(len(words)) for j in range(i + 1, len(words)) if words[i] == words[j][::-1]])
+        #return sum([1 for i in range(len(words)) for j in range(i + 1, len(words)) if words[i] == words[j][::-1]])
+        ans, a_set = 0, set()
+        for word in words:
+            if word[::-1] in a_set:
+                ans += 1
+            elif word not in a_set:
+                a_set.add(word)
+        return ans
+            
+            
+        
         
