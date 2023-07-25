6cd2d8f83e4d9b79b9304a8af55b8cae18fe379b
정재원 <Woodcook486@naver.com>
2023-07-25 21:45:39 +0900

    Time: 107 ms (96.84%), Space: 16.4 MB (59.87%) - LeetHub

--- 2788-split-strings-by-separator/2788-split-strings-by-separator.py
+++ 2788-split-strings-by-separator/2788-split-strings-by-separator.py
@@ -1,7 +1,16 @@
 class Solution:
     def splitWordsBySeparator(self, words: List[str], separator: str) -> List[str]:
+        """
         ans = list()
         for word in words:
             ans.extend(word.split(separator))
             ans = [a for a in ans if a != ""]
         return ans
+        """
+        ans = list()
+        for word in words:
+            for w in word.split(separator):
+                if w != "":
+                    ans.append(w)
+        return ans
+        
