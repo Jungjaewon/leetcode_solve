d5c63a4bfc22e1c5a41021bd3d611014892e4097
정재원 <Woodcook486@naver.com>
2023-07-27 22:20:29 +0900

    Time: 174 ms (100.00%), Space: 18.9 MB (43.77%) - LeetHub

---
+++ 0922-sort-array-by-parity-ii/0922-sort-array-by-parity-ii.py
@@ -0,0 +1,14 @@
+class Solution:
+    def sortArrayByParityII(self, nums: List[int]) -> List[int]:
+        odd_l, even_l = [n for n in nums if n & 1 == 1], [n for n in nums if n & 1 == 0]
+        ans = list()
+        odd_cnt, even_cnt = 0,0
+        for i in range(len(nums)):
+            if i & 1 == 1:
+                ans.append(odd_l[odd_cnt])
+                odd_cnt += 1
+            else:
+                ans.append(even_l[even_cnt])
+                even_cnt += 1
+        return ans
+        
