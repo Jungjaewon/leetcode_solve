9d3a3f0938c353bdbfa77b77390736b1f6bb8891
정재원 <Woodcook486@naver.com>
2023-07-28 23:21:37 +0900

    Time: 40 ms (94.01%), Space: 16.3 MB (30.67%) - LeetHub

--- 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.py
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.py
@@ -1,5 +1,6 @@
 class Solution:
     def totalMoney(self, n: int) -> int:
+        """
         nw, ret, ans = n // 7, n % 7, 0
         cnt = 1
         for i in range(1, nw + 1):
@@ -9,4 +10,14 @@ class Solution:
         for j in range(cnt, cnt + ret):
             ans += j
         return int(ans)
+        """
+        nw, ret, ans = n // 7, n % 7, 0
+        cnt = 1
+        for i in range(1, nw + 1):
+            for j in range(cnt, cnt + 7):
+                ans += j
+            cnt += 1
+        for j in range(cnt, cnt + ret):
+            ans += j
+        return ans
             
