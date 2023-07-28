d6a81985c4d5d54ca3b52afc44da8a35901d6588
정재원 <Woodcook486@naver.com>
2023-07-28 23:17:38 +0900

    Time: 42 ms (92.27%), Space: 16.3 MB (65.34%) - LeetHub

--- 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.py
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.py
@@ -1,16 +1,12 @@
 class Solution:
     def totalMoney(self, n: int) -> int:
         nw, ret, ans = n // 7, n % 7, 0
-        print(nw,ret)
         cnt = 1
         for i in range(1, nw + 1):
             for j in range(cnt, cnt + 7):
                 ans += j
             cnt += 1
-        print(f'ans {ans}')
-        print(f'cnt : {cnt}')
         for j in range(cnt, cnt + ret):
-            #print(j)
             ans += j
         return int(ans)
             
