e70a810857ee9b2369f964ca71611ee131ecf47a
정재원 <Woodcook486@naver.com>
2023-07-29 22:20:25 +0900

    Time: 50 ms (68.23%), Space: 16.1 MB (99.38%) - LeetHub

--- 1952-three-divisors/1952-three-divisors.py
+++ 1952-three-divisors/1952-three-divisors.py
@@ -5,4 +5,5 @@ class Solution:
         return True if cnt == 3 else False
         """
         #return True if sum([1 for i in range(1, n + 1) if n % i == 0]) == 3 else False
-        return True if len([1 for i in range(1, n + 1) if n % i == 0]) == 3 else False
+        #return True if len([1 for i in range(1, n + 1) if n % i == 0]) == 3 else False
+        return sum([1 for i in range(1, n + 1) if n % i == 0]) == 3
