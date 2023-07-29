a713d5961598fe567a5dfb9d330f87322329a675
정재원 <Woodcook486@naver.com>
2023-07-29 22:18:46 +0900

    Time: 42 ms (93.10%), Space: 16.1 MB (90.76%) - LeetHub

--- 1952-three-divisors/1952-three-divisors.py
+++ 1952-three-divisors/1952-three-divisors.py
@@ -1,4 +1,7 @@
 class Solution:
     def isThree(self, n: int) -> bool:
+        """
         cnt = sum([1 for i in range(1, n + 1) if n % i == 0])
         return True if cnt == 3 else False
+        """
+        return True if sum([1 for i in range(1, n + 1) if n % i == 0]) == 3 else False
