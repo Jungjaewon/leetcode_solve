8b5f7cd5262975b95dfcf35886b823896770317f
정재원 <Woodcook486@naver.com>
2023-07-31 20:46:26 +0900

    Time: 476 ms (5.43%), Space: 17.6 MB (22.61%) - LeetHub

--- 2319-check-if-matrix-is-x-matrix/2319-check-if-matrix-is-x-matrix.py
+++ 2319-check-if-matrix-is-x-matrix/2319-check-if-matrix-is-x-matrix.py
@@ -22,6 +22,7 @@ class Solution:
                     return False
         return True
         """
+        """
         n, m = len(grid), len(grid[0])
         x,y = 0,0
         dig_list = list()
@@ -46,6 +47,31 @@ class Solution:
                     if grid[i][j] > 0:
                         return False
                     
+        return True
+        """
+        n, m = len(grid), len(grid[0])
+        x,y = 0,0
+        dig_list = list()
+        while x < n and y < m:
+            if grid[x][y] == 0:
+                return False
+            dig_list.append(f'{x}_{y}')
+            x, y = x + 1, y + 1
+        x,y = 0, m - 1
+        while x < n and -1 < y:
+            if grid[x][y] == 0:
+                return False
+            dig_list.append(f'{x}_{y}')
+            x, y = x + 1, y - 1
+        for i in range(n):
+            for j in range(m):
+                xy = f'{i}_{j}'
+                if xy in dig_list and grid[i][j] > 0:
+                    pass
+                elif xy not in dig_list and grid[i][j] == 0:
+                    pass
+                else:
+                    return False
         return True
         
             
