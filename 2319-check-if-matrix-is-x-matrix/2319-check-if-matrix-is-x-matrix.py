92f2b8af0c750cc0ef27d4055ef16a5e77a81fd9
정재원 <Woodcook486@naver.com>
2023-07-31 20:37:17 +0900

    Time: 466 ms (5.43%), Space: 17.5 MB (22.61%) - LeetHub

--- 2319-check-if-matrix-is-x-matrix/2319-check-if-matrix-is-x-matrix.py
+++ 2319-check-if-matrix-is-x-matrix/2319-check-if-matrix-is-x-matrix.py
@@ -1,5 +1,6 @@
 class Solution:
     def checkXMatrix(self, grid: List[List[int]]) -> bool:
+        """
         n, m = len(grid), len(grid[0])
         x,y = 0,0
         dig_list = list()
@@ -8,6 +9,31 @@ class Solution:
             x, y = x + 1, y + 1
         x,y = 0, m - 1
         while x < n and -1 < y:
+            dig_list.append(f'{x}_{y}')
+            x, y = x + 1, y - 1
+        for i in range(n):
+            for j in range(m):
+                xy = f'{i}_{j}'
+                if xy in dig_list and grid[i][j] > 0:
+                    pass
+                elif xy not in dig_list and grid[i][j] == 0:
+                    pass
+                else:
+                    return False
+        return True
+        """
+        n, m = len(grid), len(grid[0])
+        x,y = 0,0
+        dig_list = list()
+        while x < n and y < m:
+            if grid[x][y] == 0:
+                return False
+            dig_list.append(f'{x}_{y}')
+            x, y = x + 1, y + 1
+        x,y = 0, m - 1
+        while x < n and -1 < y:
+            if grid[x][y] == 0:
+                return False
             dig_list.append(f'{x}_{y}')
             x, y = x + 1, y - 1
         for i in range(n):
