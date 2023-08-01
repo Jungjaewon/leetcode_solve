1e65746a01f31e3487689099b88cc0b0a7781795
정재원 <Woodcook486@naver.com>
2023-08-01 22:07:05 +0900

    Time: 51 ms (71.28%), Space: 16.4 MB (54.02%) - LeetHub

--- 2309-greatest-english-letter-in-upper-and-lower-case/2309-greatest-english-letter-in-upper-and-lower-case.py
+++ 2309-greatest-english-letter-in-upper-and-lower-case/2309-greatest-english-letter-in-upper-and-lower-case.py
@@ -1,5 +1,6 @@
 class Solution:
     def greatestLetter(self, s: str) -> str:
+        """
         ans = list()
         u_set, l_set = set(), set()
         for c in s:
@@ -12,5 +13,16 @@ class Solution:
                 ans.append(uc)
         if len(ans) > 1:
             ans = sorted(ans)
-            
         return "" if len(ans) == 0 else f'{ans[-1]}'
+        """
+        ans = list()
+        u_set, l_set = set(), set()
+        for c in s:
+            if c.isupper():
+                u_set.add(c)
+            else:
+                l_set.add(c)
+        for uc in u_set:
+            if uc.lower() in l_set:
+                ans.append(uc)
+        return "" if len(ans) == 0 else f'{sorted(ans)[-1]}'
