d33ec3533d3ce21d919e2802fa5c2e934190a485
정재원 <Woodcook486@naver.com>
2023-08-01 22:03:18 +0900

    Time: 44 ms (87.95%), Space: 16.4 MB (54.02%) - LeetHub

---
+++ 2309-greatest-english-letter-in-upper-and-lower-case/2309-greatest-english-letter-in-upper-and-lower-case.py
@@ -0,0 +1,16 @@
+class Solution:
+    def greatestLetter(self, s: str) -> str:
+        ans = list()
+        u_set, l_set = set(), set()
+        for c in s:
+            if c.isupper():
+                u_set.add(c)
+            else:
+                l_set.add(c)
+        for uc in u_set:
+            if uc.lower() in l_set:
+                ans.append(uc)
+        if len(ans) > 1:
+            ans = sorted(ans)
+            
+        return "" if len(ans) == 0 else f'{ans[-1]}'
