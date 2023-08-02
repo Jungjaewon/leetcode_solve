fd477740d7c82400f1ee5cb65c755dd2de1effd4
정재원 <Woodcook486@naver.com>
2023-08-02 18:46:48 +0900

    Time: 165 ms (61.55%), Space: 17 MB (37.61%) - LeetHub

--- 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.py
+++ 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.py
@@ -1,9 +1,25 @@
 class Solution:
     def countCharacters(self, words: List[str], chars: str) -> int:
+        """
         chars_d, ans = dict(Counter(chars)), 0
         for word in words:
             flag = True
             temp_d = dict(Counter(word))
+            for c in temp_d:
+                if c in chars_d:
+                    if not (temp_d[c] <= chars_d[c]):
+                        flag = False
+                        break
+                else:
+                    flag = False
+                    break
+            if flag:
+                ans += len(word)
+        return ans
+        """
+        chars_d, ans = dict(Counter(chars)), 0
+        for word in words:
+            flag, temp_d = True, dict(Counter(word))
             for c in temp_d:
                 if c in chars_d:
                     if not (temp_d[c] <= chars_d[c]):
