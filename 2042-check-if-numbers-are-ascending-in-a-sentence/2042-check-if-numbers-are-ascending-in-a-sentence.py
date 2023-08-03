255c229b3bddd3f4dcb6fb3c4b52864dcbb4f25c
정재원 <Woodcook486@naver.com>
2023-08-04 00:01:47 +0900

    Time: 45 ms (60.79%), Space: 16.2 MB (95.23%) - LeetHub

--- 2042-check-if-numbers-are-ascending-in-a-sentence/2042-check-if-numbers-are-ascending-in-a-sentence.py
+++ 2042-check-if-numbers-are-ascending-in-a-sentence/2042-check-if-numbers-are-ascending-in-a-sentence.py
@@ -4,8 +4,17 @@ class Solution:
         a_l = [ int(w) for w in s.split() if w.isdigit()]
         return a_l == sorted(a_l) if len(set(a_l)) != 1 and not len(set(a_l)) < len(a_l) else False
         """
+        """
         a_l = [ int(w) for w in s.split() if w.isdigit()]
         for idx, n in enumerate(a_l[:-1]):
             if not a_l[idx] < a_l[idx + 1]:
                 return False
+        return True
+        """
+        base = -1
+        for idx, n in enumerate([int(w) for w in s.split() if w.isdigit()]):
+            if not base < n:
+                return False
+            else:
+                base = n
         return True
