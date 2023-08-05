56e6da6e3280a2b49052d76915d9441aa3dd241a
정재원 <Woodcook486@naver.com>
2023-08-05 20:51:24 +0900

    Time: 303 ms (60.50%), Space: 16.3 MB (66.30%) - LeetHub

--- 1925-count-square-sum-triples/1925-count-square-sum-triples.py
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples.py
@@ -1,5 +1,15 @@
 class Solution:
     def countTriples(self, n: int) -> int:
+        """
+        https://leetcode.com/problems/count-square-sum-triples/discuss/2677699/pythonororeasy
+        x=0
+			for i in range(1,n-1):
+				for j in range(i+1,n):
+					tri=math.sqrt((i**2+j**2))
+					if tri<=n and int(tri)==tri :
+						x+=2
+			return x
+        """
         ans = 0
         for a in range(1, n + 1):
             for b in range(1, n + 1):
