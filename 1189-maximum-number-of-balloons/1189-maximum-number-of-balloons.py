0f7507019b598a0d1485a1f064759be7aac7e81d
정재원 <Woodcook486@naver.com>
2023-08-08 18:43:16 +0900

    Time: 46 ms (67.27%), Space: 16.2 MB (93.58%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
@@ -1,5 +1,6 @@
 class Solution:
     def maxNumberOfBalloons(self, text: str) -> int:
+        """
         s_cnt,t_cnt, ans = Counter('balloon'), Counter(text), list()
         for c in s_cnt:
             if c in t_cnt:
@@ -7,6 +8,12 @@ class Solution:
             else:
                 ans.append(0)
         return min(ans)
+        """
+        s_cnt,t_cnt, ans = Counter('balloon'), Counter(text), list()
+        for c in s_cnt:
+            if c in t_cnt:
+                ans.append(t_cnt[c] // s_cnt[c])
+        return min(ans) if len(s_cnt) == len(ans) else 0
         
         
         
