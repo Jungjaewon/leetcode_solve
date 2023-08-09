24f190f9bfa7e2c507ef21da21c76b6f974bd679
정재원 <Woodcook486@naver.com>
2023-08-09 18:32:32 +0900

    Time: 42 ms (68.39%), Space: 16.2 MB (68.74%) - LeetHub

--- 1507-reformat-date/1507-reformat-date.py
+++ 1507-reformat-date/1507-reformat-date.py
@@ -1,5 +1,10 @@
 class Solution:
     def reformatDate(self, date: str) -> str:
+        """
         d, m, y = date.split(' ')
         m_dict = {'Jan' : 1, 'Feb': 2, 'Mar':3, 'Apr':4, 'May':5, 'Jun':6, 'Jul':7, 'Aug':8, 'Sep':9, 'Oct':10, 'Nov':11, 'Dec':12}
         return f'{y}-{str(m_dict[m]).zfill(2)}-{str(d[:-2]).zfill(2)}'
+        """
+        d, m, y = date.split(' ')
+        m_dict = {'Jan' : '01', 'Feb': '02', 'Mar':'03', 'Apr':'04', 'May':'05', 'Jun':'06', 'Jul':'07', 'Aug':'08', 'Sep':'09', 'Oct':'10', 'Nov':'11', 'Dec':'12'}
+        return f'{y}-{m_dict[m]}-{str(d[:-2]).zfill(2)}'
