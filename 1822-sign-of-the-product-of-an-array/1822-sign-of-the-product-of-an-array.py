a5e8381e60a777b475f16e7eb2d1c4a206233414
정재원 <Woodcook486@naver.com>
2023-08-10 17:57:46 +0900

    Time: 64 ms (88.19%), Space: 16.2 MB (99.33%) - LeetHub

--- 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.py
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.py
@@ -1,5 +1,6 @@
 class Solution:
     def arraySign(self, nums: List[int]) -> int:
+        """
         n = nums[0]
         for num in nums[1:]:
             n  *= num
@@ -9,3 +10,8 @@ class Solution:
             return -1
         else:
             return 0
+        """
+        n = nums[0]
+        for num in nums[1:]:
+            n  *= num
+        return 1 if n > 0 else -1 if n < 0 else 0
