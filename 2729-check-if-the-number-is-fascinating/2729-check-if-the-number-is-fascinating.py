2552ba0428163b4c94562b85e3a8ee17f8f01a41
정재원 <Woodcook486@naver.com>
2023-08-11 19:57:17 +0900

    Time: 47 ms (62.82%), Space: 16.3 MB (67.67%) - LeetHub

--- 2729-check-if-the-number-is-fascinating/2729-check-if-the-number-is-fascinating.py
+++ 2729-check-if-the-number-is-fascinating/2729-check-if-the-number-is-fascinating.py
@@ -11,12 +11,23 @@ class Solution:
                     return False
         return True
         """
+        """
         c = Counter(str(n) + str(n * 2) + str(n * 3))
         for i in range(0, 10):
             if "0" in c:
                 return False
             elif '1' <= str(i) and str(i) <= '9':
                 if c[str(i)] != 1:
+                    return False
+        return True
+        """
+        c = Counter(str(n) + str(n * 2) + str(n * 3))
+        for i in range(0, 10):
+            i = str(i)
+            if "0" in c:
+                return False
+            elif '1' <= i and i <= '9':
+                if c[i] != 1:
                     return False
         return True
         
