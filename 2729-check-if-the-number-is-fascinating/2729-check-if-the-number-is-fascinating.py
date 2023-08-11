29c076c8c02d0c03f7e36f4ff55a7482875e2af0
정재원 <Woodcook486@naver.com>
2023-08-11 19:56:29 +0900

    Time: 37 ms (95.40%), Space: 16.2 MB (67.67%) - LeetHub

--- 2729-check-if-the-number-is-fascinating/2729-check-if-the-number-is-fascinating.py
+++ 2729-check-if-the-number-is-fascinating/2729-check-if-the-number-is-fascinating.py
@@ -1,8 +1,8 @@
 class Solution:
     def isFascinating(self, n: int) -> bool:
+        """
         a, b = n * 2, n * 3
         c = Counter(str(n) + str(a) + str(b))
-        
         for i in range(0, 10):
             if "0" in c:
                 return False
@@ -10,5 +10,13 @@ class Solution:
                 if c[str(i)] != 1:
                     return False
         return True
-            
-                
+        """
+        c = Counter(str(n) + str(n * 2) + str(n * 3))
+        for i in range(0, 10):
+            if "0" in c:
+                return False
+            elif '1' <= str(i) and str(i) <= '9':
+                if c[str(i)] != 1:
+                    return False
+        return True
+        
