26796af714b7f302abe6b4103d15d02a42544710
정재원 <Woodcook486@naver.com>
2023-08-12 19:03:33 +0900

    Time: 50 ms (33.38%), Space: 16.1 MB (99.46%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.py
+++ 0367-valid-perfect-square/0367-valid-perfect-square.py
@@ -4,4 +4,11 @@ class Solution:
         s = int(sqrt(num))
         return s * s == num
         """
-        return int(sqrt(num)) * int(sqrt(num)) == num
+        #return int(sqrt(num)) * int(sqrt(num)) == num
+        """
+        #Ref : https://leetcode.com/problems/valid-perfect-square/discuss/3666963/PerfectSquare-oror-Best-Solution-oror-With-Explanation-oror-Intuition-%2B-Approach-%2B-Code
+        """
+        i = 0
+        while i * i < num:
+            i += 1
+        return i * i == num
