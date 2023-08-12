2f818291b2e642bbb59f9224692f0d3f1c52dca1
정재원 <Woodcook486@naver.com>
2023-08-12 19:10:17 +0900

    Time: 35 ms (91.77%), Space: 16.1 MB (99.46%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.py
+++ 0367-valid-perfect-square/0367-valid-perfect-square.py
@@ -7,8 +7,24 @@ class Solution:
         #return int(sqrt(num)) * int(sqrt(num)) == num
         """
         #Ref : https://leetcode.com/problems/valid-perfect-square/discuss/3666963/PerfectSquare-oror-Best-Solution-oror-With-Explanation-oror-Intuition-%2B-Approach-%2B-Code
-        """
         i = 0
         while i * i < num:
             i += 1
         return i * i == num
+        """
+        # Ref https://leetcode.com/problems/valid-perfect-square/discuss/3285735/Beginner-friendly-in-python
+        if num==1:
+            return True
+        else:
+            l,r = 0, num
+            while l<r:
+                mid=(l+r)//2
+                if mid *mid ==num:
+                    return True
+                elif mid*mid>num:
+                    r=mid
+                else:
+                    l=mid+1
+            return False
+        
+        
