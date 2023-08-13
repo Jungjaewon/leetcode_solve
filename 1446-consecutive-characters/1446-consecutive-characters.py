58c600f754b64d08688dd6740bc0f992de4cc9a7
정재원 <Woodcook486@naver.com>
2023-08-13 21:05:46 +0900

    Time: 47 ms (85.11%), Space: 16.4 MB (37.61%) - LeetHub

--- 1446-consecutive-characters/1446-consecutive-characters.py
+++ 1446-consecutive-characters/1446-consecutive-characters.py
@@ -1,5 +1,6 @@
 class Solution:
     def maxPower(self, s: str) -> int:
+        """
         ans, temp_c, cnt = list(), '', 0
         for c in s:
             if temp_c == '':
@@ -11,4 +12,18 @@ class Solution:
                 temp_c, cnt = c, 1
         ans.append(cnt)
         return max(ans)
+        """
+        ans, temp_c, cnt = list(), '', 0
+        for c in s:
+            if temp_c == '':
+                temp_c, cnt = c, 1
+            elif temp_c == c:
+                cnt += 1
+            else:
+                ans.append(cnt)
+                temp_c, cnt = c, 1
+        if cnt > 0:
+            ans.append(cnt)
+        return max(ans)
+        
         
