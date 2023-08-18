f41b892a69b28a6cd6c3f45dc8e1c9e843284d90
정재원 <Woodcook486@naver.com>
2023-08-18 23:14:18 +0900

    Time: 131 ms (70.65%), Space: 16.6 MB (16.02%) - LeetHub

--- 2441-largest-positive-integer-that-exists-with-its-negative/2441-largest-positive-integer-that-exists-with-its-negative.py
+++ 2441-largest-positive-integer-that-exists-with-its-negative/2441-largest-positive-integer-that-exists-with-its-negative.py
@@ -6,11 +6,19 @@ class Solution:
                 return n
         return -1
         """
+        """
         a_dict, ans = defaultdict(list), -1
         for n in nums:
             a_dict[abs(n)].append(n)
-        
         for n in a_dict:
             if len(a_dict[n]) >= 2 and n in a_dict[n] and -n in a_dict[n]:
+                ans = max(ans, n)
+        return ans
+        """
+        a_set, ans = set(), -1
+        for n in nums:
+            a_set.add(n)
+        for n in nums:
+            if n in a_set and -n in a_set:
                 ans = max(ans, n)
         return ans
