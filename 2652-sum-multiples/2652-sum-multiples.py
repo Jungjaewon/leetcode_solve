9ae0c985d1e01bd62edd72335306a7f8f7e49130
정재원 <Woodcook486@naver.com>
2023-08-21 00:09:18 +0900

    Time: 44 ms (98.79%), Space: 16.3 MB (24.05%) - LeetHub

--- 2652-sum-multiples/2652-sum-multiples.py
+++ 2652-sum-multiples/2652-sum-multiples.py
@@ -7,4 +7,11 @@ class Solution:
                 ans += num
         return ans
         """
-        return sum([num for num in range(1, n + 1) if num % 3 == 0 or num % 5 == 0 or num % 7 == 0])
+        # https://leetcode.com/problems/sum-multiples/discuss/3445711/Kotlin-O(1)-with-Diagram
+        #return sum([num for num in range(1, n + 1) if num % 3 == 0 or num % 5 == 0 or num % 7 == 0])
+        
+        def count(value):
+            low, high = value, n // value * value
+            count = n // value
+            return (low + high) * count // 2
+        return count(3) + count(5) + count(7) - (count(15) + count(21) + count(35)) + count(105)
