16249d2ce1af5702d41f3d70a1156f481d0ce6dc
정재원 <Woodcook486@naver.com>
2023-08-22 18:28:51 +0900

    Time: 63 ms (17.56%), Space: 16.2 MB (71.34%) - LeetHub

--- 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.py
+++ 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.py
@@ -16,4 +16,6 @@ class Solution:
                     ans +=1
         return ans // 2
         """
-        return sum([1 for i in range(len(nums)) for j in range(i + 1, len(nums)) if i != j and nums[i] + nums[j] < target])
+        #return sum([1 for i in range(len(nums)) for j in range(i + 1, len(nums)) if i != j and nums[i] + nums[j] < target])
+        # https://leetcode.com/problems/count-pairs-whose-sum-is-less-than-target/discuss/3934543/Python-3-oror-1-line-combinations-oror-TM%3A-54-ms-16-MB
+        return sum(map(lambda x: x[0]+x[1] < target, combinations(nums, 2)))
