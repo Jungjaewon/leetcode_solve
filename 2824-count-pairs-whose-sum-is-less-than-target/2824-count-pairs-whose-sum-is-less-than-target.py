7ec70239dd337ddd6d3b1d32c2eca56e5db332a3
정재원 <Woodcook486@naver.com>
2023-08-22 18:47:39 +0900

    Time: 57 ms (48.96%), Space: 16.3 MB (71.34%) - LeetHub

--- 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.py
+++ 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.py
@@ -18,4 +18,18 @@ class Solution:
         """
         #return sum([1 for i in range(len(nums)) for j in range(i + 1, len(nums)) if i != j and nums[i] + nums[j] < target])
         # https://leetcode.com/problems/count-pairs-whose-sum-is-less-than-target/discuss/3934543/Python-3-oror-1-line-combinations-oror-TM%3A-54-ms-16-MB
-        return sum(map(lambda x: x[0]+x[1] < target, combinations(nums, 2)))
+        #return sum(map(lambda x: x[0]+x[1] < target, combinations(nums, 2)))
+        #https://leetcode.com/problems/count-pairs-whose-sum-is-less-than-target/discuss/3937200/Python-Elegant-and-Short-or-Brute-Force-vs-Greedy-or-O(n*log(n))
+        nums.sort()
+
+        lo, hi = 0, len(nums) - 1
+        pairs = 0
+
+        while lo < hi:
+            if nums[lo] + nums[hi] < target:
+                pairs += hi - lo
+                lo += 1
+            else:
+                hi -= 1
+
+        return pairs
