49f6d3f6b7de83ef58407b283b9505a1dad1075b
정재원 <Woodcook486@naver.com>
2023-08-22 18:26:07 +0900

    Time: 67 ms (5.69%), Space: 16.2 MB (71.34%) - LeetHub

--- 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.py
+++ 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.py
@@ -8,9 +8,12 @@ class Solution:
                     ans +=1
         return ans
         """
+        """
         ans = 0
         for i in range(len(nums)):
             for j in range(len(nums)):
                 if i != j and nums[i] + nums[j] < target:
                     ans +=1
         return ans // 2
+        """
+        return sum([1 for i in range(len(nums)) for j in range(i + 1, len(nums)) if i != j and nums[i] + nums[j] < target])
