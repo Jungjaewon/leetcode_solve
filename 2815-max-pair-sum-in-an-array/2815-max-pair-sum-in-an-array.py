c7ebef36a798223c7672006fb3b432c850eeb360
정재원 <Woodcook486@naver.com>
2023-08-27 10:01:57 +0900

    Time: 706 ms (25.95%), Space: 16.9 MB (5.77%) - LeetHub

--- 2815-max-pair-sum-in-an-array/2815-max-pair-sum-in-an-array.py
+++ 2815-max-pair-sum-in-an-array/2815-max-pair-sum-in-an-array.py
@@ -1,7 +1,14 @@
 class Solution:
     def maxSum(self, nums: List[int]) -> int:
+        """
         ans = -1
         for a, b in list(combinations(nums, 2)):
             if sorted(list(str(a)))[-1] == sorted(list(str(b)))[-1]:
+                ans = max(ans, a + b)
+        return ans
+        """
+        ans = -1
+        for a, b in list(combinations(nums, 2)):
+            if max(list(str(a))) == max(list(str(b))):
                 ans = max(ans, a + b)
         return ans
