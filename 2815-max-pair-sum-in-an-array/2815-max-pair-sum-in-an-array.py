d803ed2ffad72dd63921482fee811f25c557ff63
정재원 <Woodcook486@naver.com>
2023-08-27 10:08:34 +0900

    Time: 148 ms (50.49%), Space: 16.7 MB (5.77%) - LeetHub

--- 2815-max-pair-sum-in-an-array/2815-max-pair-sum-in-an-array.py
+++ 2815-max-pair-sum-in-an-array/2815-max-pair-sum-in-an-array.py
@@ -7,8 +7,20 @@ class Solution:
                 ans = max(ans, a + b)
         return ans
         """
+        """
         ans = -1
         for a, b in list(combinations(nums, 2)):
             if max(list(str(a))) == max(list(str(b))):
                 ans = max(ans, a + b)
+        return ans
+        """
+        ans = -1
+        a_dict = defaultdict(list)
+        for num in nums:
+            a_dict[max(list(str(num)))].append(num)
+        max_key = sorted(list(a_dict.keys()))[-1]
+        for n in a_dict:
+            if len(a_dict[n])> 1:
+                for a,b in list(combinations(a_dict[n],2)):
+                    ans = max(ans, a + b)
         return ans
