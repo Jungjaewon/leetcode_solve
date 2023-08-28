8049df49cc3a56c2a6e286e40a20dc319c92dd4d
정재원 <Woodcook486@naver.com>
2023-08-28 18:39:21 +0900

    Time: 37 ms (83.01%), Space: 16.2 MB (94.75%) - LeetHub

--- 2138-divide-a-string-into-groups-of-size-k/2138-divide-a-string-into-groups-of-size-k.py
+++ 2138-divide-a-string-into-groups-of-size-k/2138-divide-a-string-into-groups-of-size-k.py
@@ -10,10 +10,18 @@ class Solution:
             ans.append(temp)
         return ans
         """
+        """
         ans = list()
         for i in range(0,len(s),k):
             temp = s[i:i+k]
             if len(temp) < k:
                 temp += fill * (k - len(temp))
+            ans.append(temp)
+        return ans
+        """
+        ans = list()
+        for i in range(0,len(s),k):
+            temp = s[i:i+k]
+            temp = temp + fill * (k - len(temp)) if len(temp) < k else temp
             ans.append(temp)
         return ans
