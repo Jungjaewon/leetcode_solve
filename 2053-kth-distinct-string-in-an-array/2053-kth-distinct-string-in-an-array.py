461354e8dfa09dec76e179b95d0a11c1eb6d4ba9
정재원 <Woodcook486@naver.com>
2023-08-29 18:37:52 +0900

    Time: 80 ms (72.72%), Space: 16.5 MB (80.29%) - LeetHub

--- 2053-kth-distinct-string-in-an-array/2053-kth-distinct-string-in-an-array.py
+++ 2053-kth-distinct-string-in-an-array/2053-kth-distinct-string-in-an-array.py
@@ -1,5 +1,6 @@
 class Solution:
     def kthDistinct(self, arr: List[str], k: int) -> str:
+        """
         ans_list, c = list(), Counter(arr)
         for n in arr:
             if c[n] == 1:
@@ -8,3 +9,7 @@ class Solution:
             return ans_list[k - 1]
         else:
             return ""
+        """
+        c = Counter(arr)
+        ans_list = [ n for n in arr if c[n] == 1]
+        return ans_list[k - 1] if k <= len(ans_list) else ""
