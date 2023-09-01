ef1f4d9e9ab08d3dfaa480a2b3d678fbea740811
정재원 <Woodcook486@naver.com>
2023-09-01 22:11:24 +0900

    Time: 54 ms (77.10%), Space: 16.4 MB (78.28%) - LeetHub

--- 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.py
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.py
@@ -40,10 +40,31 @@ class Solution:
         return ans
         """
         # https://leetcode.com/problems/final-prices-with-a-special-discount-in-a-shop/discuss/685578/Python-31-pass-O(n)-code-using-stack-w-brief-explanation-and-analysis.
+        """
         res, stack = prices[:], []
         for i, price in enumerate(prices):
             while stack and prices[stack[-1]] >= price:
                 res[stack.pop()] -= price
             stack.append(i)
         return res
+        """
+        i = 0 
+        j = i +1
+
+        while i <j and j < len(prices) :
+            #STEP 1: INCREASE I TO  NEXT INDEX AND J NEXT TO I IF ITH ELEMENT GREATER THAN JTH AFTER PERFORMING THE OPERATION
+            if prices[i] >= prices[j]:
+                prices[i] = prices[i] - prices[j]
+                i = i +1
+                j = i+1
+            else:
+            #STEP2:  IF THERE IS NO JTH ELEMENT WHICHH IS SMALLER THAN ITH AND YOU REACH END OF ARRAY THEN INCREMENT i and shift back j to i+1
+                if (j == len(prices)-1):
+                    i = i +1
+                    j = i +1
+
+            # STEP3 :ELSE INCREASE j by 1 and re enter in to step 1 if condition satisfies
+                else:
+                    j = j +1
+        return prices
     
