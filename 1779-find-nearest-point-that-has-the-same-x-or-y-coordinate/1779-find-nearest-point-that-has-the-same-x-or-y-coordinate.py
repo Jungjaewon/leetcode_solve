f8d315e766c97ce8ae003ed738c7d44867c08994
정재원 <Woodcook486@naver.com>
2023-09-03 17:22:40 +0900

    Time: 644 ms (85.51%), Space: 22.2 MB (50.93%) - LeetHub

--- 1779-find-nearest-point-that-has-the-same-x-or-y-coordinate/1779-find-nearest-point-that-has-the-same-x-or-y-coordinate.py
+++ 1779-find-nearest-point-that-has-the-same-x-or-y-coordinate/1779-find-nearest-point-that-has-the-same-x-or-y-coordinate.py
@@ -1,9 +1,24 @@
 class Solution:
     def nearestValidPoint(self, x: int, y: int, points: List[List[int]]) -> int:
+        """
         ans = list()
         for idx, ab in enumerate(points):
             a,b = ab
             if x == a or y == b:
                 ans.append([idx, abs(x - a) + abs(y - b)])
         return -1 if len(ans) == 0 else sorted(ans, key=lambda x : (x[1],x[0]))[0][0]
+        """
+        a_idx, ans = -1, -1
+        for idx, ab in enumerate(points):
+            a,b = ab
+            if x == a or y == b:
+                dis = abs(x - a) + abs(y - b)
+                if ans == -1:
+                    a_idx, ans = idx, dis
+                else:
+                    if dis < ans:
+                        a_idx, ans = idx, min(dis, ans)
+                    elif dis == ans:
+                        a_idx = min(idx, a_idx)
+        return a_idx
                 
