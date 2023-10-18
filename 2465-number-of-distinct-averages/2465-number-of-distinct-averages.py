c4669b0718b5def8faccdb1235bb3afd3ba20185
정재원 <Woodcook486@naver.com>
2023-10-18 22:00:28 +0900

    Time: 31 ms (94.76%), Space: 16.3 MB (16.51%) - LeetHub

--- 2465-number-of-distinct-averages/2465-number-of-distinct-averages.py
+++ 2465-number-of-distinct-averages/2465-number-of-distinct-averages.py
@@ -1,13 +1,17 @@
 class Solution:
     def distinctAverages(self, nums: List[int]) -> int:
-        ans = set()
-        while len(nums):
-            x_min = min(nums)
-            nums.remove(x_min)
-            x_max = max(nums)
-            nums.remove(x_max)
-            ans.add((x_min + x_max) / 2)
-        #counter = Counter(ans)
-        #print(counter)
-        return len(ans)
+        
+        if len(nums) == 2:
+            return 1
+        else:
+            ans = set()
+            while len(nums):
+                x_min = min(nums)
+                nums.remove(x_min)
+                x_max = max(nums)
+                nums.remove(x_max)
+                ans.add((x_min + x_max) / 2)
+            #counter = Counter(ans)
+            #print(counter)
+            return len(ans)
             
