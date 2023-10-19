70ced1f1154920b7cd1226327d161b2524f2aeb0
정재원 <Woodcook486@naver.com>
2023-10-19 18:57:38 +0900

    Time: 257 ms (95.74%), Space: 35.9 MB (28.27%) - LeetHub

---
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.py
@@ -0,0 +1,8 @@
+class Solution:
+    def arrayRankTransform(self, arr: List[int]) -> List[int]:
+        rank_dict = { n: (i + 1) for i, n in enumerate(sorted(list(set(arr))))}
+        ans = list()
+        for n in arr:
+            ans.append(rank_dict[n])
+        return ans
+        
