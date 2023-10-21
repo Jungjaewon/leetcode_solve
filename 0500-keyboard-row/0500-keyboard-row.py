81e2809b3cb3546ec97cff3913197b4cba09b79d
정재원 <Woodcook486@naver.com>
2023-10-21 18:38:37 +0900

    Time: 36 ms (72.15%), Space: 16.1 MB (77.34%) - LeetHub

---
+++ 0500-keyboard-row/0500-keyboard-row.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findWords(self, words: List[str]) -> List[str]:
+        a = "qwertyuiop"
+        b = "asdfghjkl"
+        c = "zxcvbnm"
+        ans = list()
+        for row in [a,b,c]:
+            for word in words:
+                flag = True
+                for c in word:
+                    if c.lower() not in row:
+                        flag = False
+                        break
+                if flag:
+                    ans.append(word)
+        return ans
