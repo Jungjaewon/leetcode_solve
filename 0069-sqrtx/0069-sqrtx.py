08b9716a640359f3e0dc7636351d870f8dda5f03
정재원 <Woodcook486@naver.com>
2023-10-23 19:18:50 +0900

    Time: 1863 ms (10.12%), Space: 16.3 MB (39.18%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -3,12 +3,7 @@ class Solution:
         """
         import math
         return int(math.sqrt(x))
-        """
-        if x in [0, 1]:
-            return x
-        elif x in [2,3]:
-            return 1
-            
+        """            
         for i in range(0, x + 1):
             if (i * i) == x:
                 return i
