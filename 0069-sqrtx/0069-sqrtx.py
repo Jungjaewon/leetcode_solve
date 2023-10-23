544070419627fd6a264d3eb66638ab11c0b13503
정재원 <Woodcook486@naver.com>
2023-10-23 19:19:27 +0900

    Time: 1640 ms (11.59%), Space: 16.2 MB (39.18%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -4,8 +4,16 @@ class Solution:
         import math
         return int(math.sqrt(x))
         """            
+        """
         for i in range(0, x + 1):
             if (i * i) == x:
                 return i
             elif (i * i) > x:
+                return i - 1
+        """
+        for i in range(0, x + 1):
+            ret = (i * i)
+            if ret == x:
+                return i
+            elif ret > x:
                 return i - 1
