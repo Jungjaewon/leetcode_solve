f367d7382811eb106798fe40a7828cd9cc1ddfeb
정재원 <Woodcook486@naver.com>
2023-10-23 19:18:39 +0900

    Time: 1929 ms (7.77%), Space: 16.2 MB (71.74%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -1,4 +1,16 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
+        """
         import math
         return int(math.sqrt(x))
+        """
+        if x in [0, 1]:
+            return x
+        elif x in [2,3]:
+            return 1
+            
+        for i in range(0, x + 1):
+            if (i * i) == x:
+                return i
+            elif (i * i) > x:
+                return i - 1
