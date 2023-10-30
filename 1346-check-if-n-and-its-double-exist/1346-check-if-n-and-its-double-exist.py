8c2a8641a274c3b15e8351ee7ae12f0303f28d6a
정재원 <Woodcook486@naver.com>
2023-10-30 20:07:15 +0900

    Time: 63 ms (43.95%), Space: 16.6 MB (14.81%) - LeetHub

---
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.py
@@ -0,0 +1,16 @@
+class Solution:
+    def checkIfExist(self, arr: List[int]) -> bool:
+        a_dict = defaultdict(list)
+        for i, n in enumerate(arr):
+            a_dict[n].append(i)
+        print(f'a_dict : {a_dict}')
+        for i, n in enumerate(arr):
+            key = int(n * 2)
+            idx_list = a_dict[key]
+            if key in a_dict and len(idx_list):
+                for j in idx_list:
+                    if i != j:
+                        return True
+        return False
+                
+                
