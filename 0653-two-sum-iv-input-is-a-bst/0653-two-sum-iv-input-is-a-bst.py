061d0b8e5a1087ed0fe5745ff10dc88fe2d23c67
정재원 <Woodcook486@naver.com>
2023-10-31 19:30:31 +0900

    Time: 94 ms (15.99%), Space: 22.9 MB (9.44%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.py
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.py
@@ -6,12 +6,36 @@
 #         self.right = right
 class Solution:
     def findTarget(self, root: Optional[TreeNode], k: int) -> bool:
+        """
         def func(node: Optional[TreeNode]):
             if node is None:
                 return []
             else:
                 return [node.val] + func(node.left) + func(node.right)
         numbers = dict(Counter(func(root)))
+        
+        if len(numbers) in [0,1]:
+            return False
+        
+        for n in numbers:
+            gap = k - n
+            if gap == n:
+                if gap in numbers and numbers[gap] > 1:
+                    return True
+            else:
+                 if gap in numbers:
+                    return True
+        return False
+        """
+        def func(node: Optional[TreeNode]):
+            if node is None:
+                return []
+            else:
+                return [node.val] + func(node.left) + func(node.right)
+        result = func(root)
+        numbers = defaultdict(int)
+        for n in result:
+            numbers[n] += 1
         
         if len(numbers) in [0,1]:
             return False
