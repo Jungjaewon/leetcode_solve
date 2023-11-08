a9fe2d9e7258f28f71f8870cbf6142b5b717cf5e
정재원 <Woodcook486@naver.com>
2023-11-08 21:05:47 +0900

    Time: 31 ms (97.73%), Space: 16.2 MB (35.00%) - LeetHub

--- 1732-find-the-highest-altitude/1732-find-the-highest-altitude.py
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.py
@@ -1,8 +1,16 @@
 class Solution:
     def largestAltitude(self, gain: List[int]) -> int:
+        """
         #gain.insert(0,0)
         ans = [0]
         for i in range(len(gain)):
             ans.append(ans[-1] + gain[i])
         return max(ans)
+        """
+        ans,t = 0,0
+        for i in range(len(gain)):
+            t = t + gain[i]
+            ans = max(ans, t)
+            
+        return ans
         
