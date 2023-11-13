f61533ff347129ce02d59671bfdf25df8c78a6c0
정재원 <Woodcook486@naver.com>
2023-11-13 20:37:02 +0900

    Time: 148 ms (78.23%), Space: 16.1 MB (94.41%) - LeetHub

--- 1995-count-special-quadruplets/1995-count-special-quadruplets.py
+++ 1995-count-special-quadruplets/1995-count-special-quadruplets.py
@@ -1,5 +1,6 @@
 class Solution:
     def countQuadruplets(self, nums: List[int]) -> int:
+        """
         ans = 0
         for a in range(len(nums)):
             for b in range(a + 1, len(nums)):
@@ -7,5 +8,16 @@ class Solution:
                     for d in range(c + 1, len(nums)):
                         if nums[a] + nums[b] + nums[c] == nums[d]:
                             ans += 1
+        return ans
+        """
+        ans = 0 
+        check_dict = defaultdict(int)
+        for a in range(len(nums)-1,-1,-1):
+            check_dict[nums[a]] += 1
+            for b in range(a-1,-1,-1):
+                for c in range(b-1,-1,-1):
+                    temp_sum = nums[a] + nums[b] + nums[c]
+                    if temp_sum in check_dict:
+                        ans += check_dict[temp_sum]
         return ans
     #https://leetcode.com/problems/count-special-quadruplets/discuss/1456709/Python-99-Clean-Code-Walkthrough-From-O(4)-greater-O(3)-greater-O(2)
