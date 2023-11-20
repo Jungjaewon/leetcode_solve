0f277140349cb2b7b523474ca50d5821adfc8d30
정재원 <Woodcook486@naver.com>
2023-11-20 20:49:38 +0900

    Time: 85 ms (52.77%), Space: 16.6 MB (11.61%) - LeetHub

---
+++ 2190-most-frequent-number-following-key-in-an-array/2190-most-frequent-number-following-key-in-an-array.py
@@ -0,0 +1,9 @@
+class Solution:
+    def mostFrequent(self, nums: List[int], key: int) -> int:
+        count_list = list()
+        for i in range(len(nums) - 1):
+            if nums[i] == key:
+                count_list.append(nums[i + 1])
+        counter_list = Counter(count_list).items()
+        print(counter_list)
+        return sorted(counter_list, key= lambda x : -x[1])[0][0]
