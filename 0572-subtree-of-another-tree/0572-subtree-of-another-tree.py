b1003a8d2249a33458d7d6b30074bf6593319fa1
정재원 <Woodcook486@naver.com>
2023-11-27 22:43:49 +0900

    Time: 184 ms (5.13%), Space: 18.4 MB (12.93%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
@@ -6,11 +6,28 @@
 #         self.right = right
 class Solution:
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        """
         def tree2str(node : Optional[TreeNode]):
             if node is None:
                 return 'N'
             else:
                 return tree2str(node.left) + tree2str(node.right) + f'{str(node.val)}'
+        def func(node: Optional[TreeNode], sub_str: str):
+            if node is None:
+                return False
+            node_str = tree2str(node)
+            if node_str == sub_str:
+                return True
+            else:
+                return func(node.left, sub_str) or func(node.right, sub_str) 
+        sub_str = tree2str(subRoot)
+        return func(root, sub_str)
+        """
+        def tree2str(node : Optional[TreeNode]):
+            if node is None:
+                return 'N'
+            else:
+                return  f'{str(node.val)}' + tree2str(node.left) + tree2str(node.right)
         def func(node: Optional[TreeNode], sub_str: str):
             if node is None:
                 return False
