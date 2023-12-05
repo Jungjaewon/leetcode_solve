3518e3c362f0d771183b5da7f21d1e8607c5fd55
정재원 <Woodcook486@naver.com>
2023-12-05 18:40:04 +0900

    Time: 52 ms (66.56%), Space: 16.2 MB (79.01%) - LeetHub

---
+++ 2951-find-the-peaks/2951-find-the-peaks.py
@@ -0,0 +1,7 @@
+class Solution:
+    def findPeaks(self, mountain: List[int]) -> List[int]:
+        ans = list()
+        for idx in range(1, len(mountain) - 1):
+            if mountain[idx - 1] < mountain[idx] and mountain[idx + 1] < mountain[idx]:
+                ans.append(idx)
+        return ans
