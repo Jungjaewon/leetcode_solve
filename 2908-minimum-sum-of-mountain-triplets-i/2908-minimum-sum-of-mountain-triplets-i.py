8a91cefc66f50806b16ffad8be20c7de0fd0b7f8
정재원 <Woodcook486@naver.com>
2023-12-07 18:23:09 +0900

    Time: 80 ms (50.72%), Space: 16.4 MB (15.94%) - LeetHub

--- 2908-minimum-sum-of-mountain-triplets-i/2908-minimum-sum-of-mountain-triplets-i.py
+++ 2908-minimum-sum-of-mountain-triplets-i/2908-minimum-sum-of-mountain-triplets-i.py
@@ -1,8 +1,19 @@
 class Solution:
     def minimumSum(self, nums: List[int]) -> int:
+        """
         ans = 10e4
         for i in range(len(nums)):
             for j in range(i + 1, len(nums)):
+                for k in range(j + 1, len(nums)):
+                    if nums[i] < nums[j] and nums[k] < nums[j]:
+                        ans = min(ans, nums[i] + nums[j] + nums[k])
+        return -1 if ans == 10e4 else ans
+        """
+        ans = 10e4
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if not nums[i] < nums[j]:
+                    continue
                 for k in range(j + 1, len(nums)):
                     if nums[i] < nums[j] and nums[k] < nums[j]:
                         ans = min(ans, nums[i] + nums[j] + nums[k])
