5dbbd8709b66f146eb7df3137ca5b41709ae494c
정재원 <Woodcook486@naver.com>
2023-12-08 20:51:45 +0900

    Time: 214 ms (5.93%), Space: 16.3 MB (20.15%) - LeetHub

--- 2932-maximum-strong-pair-xor-i/2932-maximum-strong-pair-xor-i.py
+++ 2932-maximum-strong-pair-xor-i/2932-maximum-strong-pair-xor-i.py
@@ -3,7 +3,8 @@ class Solution:
         ans = -10e4
         for i in range(len(nums)):
             for j in range(len(nums)):
+                if nums[i] == nums[j]:
+                    continue
                 if abs(nums[i] - nums[j]) <= min(nums[i],nums[j]):
-                    ans = max(ans, nums[i] ^ nums[j])
-                
-        return ans
+                    ans = max(ans, nums[i] ^ nums[j])      
+        return ans if ans != -10e4 else 0
