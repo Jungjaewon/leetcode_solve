cfd86f309dc445d47fca9b5c7164dea5da1dc10c
정재원 <Woodcook486@naver.com>
2023-12-08 21:02:46 +0900

    Time: 135 ms (55.01%), Space: 16.4 MB (20.15%) - LeetHub

--- 2932-maximum-strong-pair-xor-i/2932-maximum-strong-pair-xor-i.py
+++ 2932-maximum-strong-pair-xor-i/2932-maximum-strong-pair-xor-i.py
@@ -1,10 +1,18 @@
 class Solution:
     def maximumStrongPairXor(self, nums: List[int]) -> int:
+        """
         ans = -10e4
         for i in range(len(nums)):
             for j in range(len(nums)):
                 if nums[i] == nums[j]:
                     continue
+                if abs(nums[i] - nums[j]) <= min(nums[i],nums[j]):
+                    ans = max(ans, nums[i] ^ nums[j])      
+        return ans if ans != -10e4 else 0
+        """
+        ans = -10e4
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
                 if abs(nums[i] - nums[j]) <= min(nums[i],nums[j]):
                     ans = max(ans, nums[i] ^ nums[j])      
         return ans if ans != -10e4 else 0
