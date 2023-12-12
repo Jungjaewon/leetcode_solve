9fe95827cd47c40e937e228630782d6182f3aaa1
정재원 <Woodcook486@naver.com>
2023-12-12 18:45:52 +0900

    Time: 171 ms (76.03%), Space: 18.6 MB (7.41%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.py
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -6,7 +6,12 @@ class Solution:
         a_1 = list(n_set - set(nums))[0]
         return [a_0, a_1]
         """
+        """
         a_0 = [k for k, v in dict(Counter(nums)).items() if v == 2][0]
+        a_1 = list(set(range(1, len(nums) + 1)) - set(nums))[0]
+        return [a_0, a_1]
+        """
+        a_0 = [k for k, v in Counter(nums).items() if v == 2][0]
         a_1 = list(set(range(1, len(nums) + 1)) - set(nums))[0]
         return [a_0, a_1]
         
