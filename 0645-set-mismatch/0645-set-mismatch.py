11b7527475a6b6d82936937c9d3362f325e15ed7
정재원 <Woodcook486@naver.com>
2023-12-12 18:41:54 +0900

    Time: 179 ms (55.35%), Space: 18.9 MB (5.52%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.py
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -1,7 +1,12 @@
 class Solution:
     def findErrorNums(self, nums: List[int]) -> List[int]:
+        """
         n_set = set(range(1, len(nums) + 1))
         a_0 = [k for k, v in dict(Counter(nums)).items() if v == 2][0]
         a_1 = list(n_set - set(nums))[0]
+        return [a_0, a_1]
+        """
+        a_0 = [k for k, v in dict(Counter(nums)).items() if v == 2][0]
+        a_1 = list(set(range(1, len(nums) + 1)) - set(nums))[0]
         return [a_0, a_1]
         
