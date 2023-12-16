688636cb09b7843b65567b9b20b44f4c1b29765c
정재원 <Woodcook486@naver.com>
2023-12-16 14:45:26 +0900

    Time: 40 ms (47.64%), Space: 16.4 MB (13.94%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.py
+++ 0844-backspace-string-compare/0844-backspace-string-compare.py
@@ -1,5 +1,6 @@
 class Solution:
     def backspaceCompare(self, s: str, t: str) -> bool:
+        """
         s_list, t_list = list(s)[::-1], list(t)[::-1]
         s_result = list()
         cnt = 0
@@ -22,4 +23,17 @@ class Solution:
                     continue
                 t_result.append(c)
         return ''.join(s_result[::-1]) == ''.join(t_result[::-1])
+        """
+        def func(str_list):
+            result, cnt = list(), 0
+            for c in str_list:
+                if c == '#':
+                    cnt += 1
+                else:
+                    if cnt:
+                        cnt -= 1
+                        continue
+                    result.append(c)
+            return result
+        return ''.join(func(list(s)[::-1])[::-1]) == ''.join(func(list(t)[::-1])[::-1])
         
