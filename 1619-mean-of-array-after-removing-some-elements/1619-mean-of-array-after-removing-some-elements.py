a4462cbec001f92f4039b96b4c2c735e05f4c638
정재원 <Woodcook486@naver.com>
2023-12-21 18:44:16 +0900

    Time: 59 ms (80.18%), Space: 17.5 MB (15.58%) - LeetHub

--- 1619-mean-of-array-after-removing-some-elements/1619-mean-of-array-after-removing-some-elements.py
+++ 1619-mean-of-array-after-removing-some-elements/1619-mean-of-array-after-removing-some-elements.py
@@ -1,10 +1,17 @@
 class Solution:
     def trimMean(self, arr: List[int]) -> float:
+        """
         cut = int(len(arr) * 0.05)
         arr = sorted(arr)
         for _ in range(cut):
             del arr[0]
         for _ in range(cut):
             del arr[-1]
+        return sum(arr) / len(arr)
+        """
+        cut = int(len(arr) * 0.05)
+        arr = sorted(arr)
+        arr = arr[cut:]
+        arr = arr[:-(cut)]
         return sum(arr) / len(arr)
         
