c543fdb2a828277c4156ad95d06c818903cc4b96
정재원 <Woodcook486@naver.com>
2023-12-23 20:11:55 +0900

    Time: 45 ms (19.23%), Space: 17.3 MB (21.72%) - LeetHub

--- 2259-remove-digit-from-number-to-maximize-result/2259-remove-digit-from-number-to-maximize-result.py
+++ 2259-remove-digit-from-number-to-maximize-result/2259-remove-digit-from-number-to-maximize-result.py
@@ -1,5 +1,6 @@
 class Solution:
     def removeDigit(self, number: str, digit: str) -> str:
+        """
         idx_list = [idx for idx, c in enumerate(number) if c == digit]
         candidates = list()
         for idx in idx_list:
@@ -7,3 +8,9 @@ class Solution:
             temp[idx] = ''
             candidates.append(''.join(temp))
         return str(max(candidates))
+        """
+        ans = -1
+        for i in range(len(number)):
+            if number[i] == digit:
+                ans = max(int(ans), int(number[:i] + number[i+1:]))
+        return str(ans)
