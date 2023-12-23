e211e7583c10f6e03490f6c06a363895fe201fe7
정재원 <Woodcook486@naver.com>
2023-12-23 20:28:27 +0900

    Time: 35 ms (81.63%), Space: 17.2 MB (21.72%) - LeetHub

--- 2259-remove-digit-from-number-to-maximize-result/2259-remove-digit-from-number-to-maximize-result.py
+++ 2259-remove-digit-from-number-to-maximize-result/2259-remove-digit-from-number-to-maximize-result.py
@@ -14,3 +14,4 @@ class Solution:
             if number[i] == digit:
                 ans = max(int(ans), int(number[:i] + number[i+1:]))
         return str(ans)
+    #https://leetcode.com/problems/remove-digit-from-number-to-maximize-result/discuss/2074599/Python-O(N)-solution-oror-Faster-than-99-submissions-oror-Detailed-explanation.
