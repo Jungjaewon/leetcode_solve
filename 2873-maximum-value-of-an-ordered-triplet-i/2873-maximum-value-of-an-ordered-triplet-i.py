c6e7d9d32372bc5423bbff576e78635b45aefaa8
정재원 <Woodcook486@naver.com>
2023-12-25 20:38:59 +0900

    Time: 65 ms (63.61%), Space: 17.9 MB (8.49%) - LeetHub

--- 2873-maximum-value-of-an-ordered-triplet-i/2873-maximum-value-of-an-ordered-triplet-i.py
+++ 2873-maximum-value-of-an-ordered-triplet-i/2873-maximum-value-of-an-ordered-triplet-i.py
@@ -17,6 +17,7 @@ class Solution:
                 ans = max(ans, (nums[i] - nums[j]) * t)
         return ans
         """
+        """
         ans = 0
         for i in range(len(nums)):
             for j in range(i + 1, len(nums)):
@@ -25,4 +26,21 @@ class Solution:
                 temp_l = nums[j + 1:]
                 t = 0 if len(temp_l) == 0 else max(temp_l)
                 ans = max(ans, (nums[i] - nums[j]) * t)
+        return ans
+        """
+        ans, ans_l = 0, list()
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] - nums[j] <= 0:
+                    continue
+                #if nums[i] - nums[j] > ans:
+                ans = nums[i] - nums[j]
+                a_idx = j
+                ans_l.append([ans, j])
+        ans = 0
+        print(ans_l)
+        for a, idx in ans_l:
+            temp_l = nums[idx + 1:]
+            t = 0 if len(temp_l) == 0 else max(temp_l)
+            ans = max(ans, a * t)
         return ans
