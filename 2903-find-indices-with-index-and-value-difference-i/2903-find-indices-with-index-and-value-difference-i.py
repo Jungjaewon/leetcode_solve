a677e1f800c67dc658ed3dffc2829550ef0e56ad
정재원 <Woodcook486@naver.com>
2023-12-30 13:40:28 +0900

    Time: 88 ms (5.05%), Space: 17.3 MB (8.25%) - LeetHub

--- 2903-find-indices-with-index-and-value-difference-i/2903-find-indices-with-index-and-value-difference-i.py
+++ 2903-find-indices-with-index-and-value-difference-i/2903-find-indices-with-index-and-value-difference-i.py
@@ -8,10 +8,27 @@ class Solution:
                     return [i, j]
         return ans
         """
+        """
         ans = [-1, -1]
         for i in range(len(nums)):
             for j in range(len(nums)):
                 if abs(i - j) >= indexDifference:
                     if abs(nums[i] - nums[j]) >= valueDifference:
                         return [i, j]
+        return ans
+        """
+        """
+        ans = [-1, -1]
+        for i in range(len(nums)):
+            for j in range(indexDifference, len(nums)):
+                if abs(i - j) >= indexDifference:
+                    if abs(nums[i] - nums[j]) >= valueDifference:
+                        return [i, j]
+        return ans
+        """
+        ans = [-1, -1]
+        for i in range(len(nums)):
+            for j in range(indexDifference + i, len(nums)):
+                if abs(nums[i] - nums[j]) >= valueDifference:
+                    return [i, j]
         return ans
