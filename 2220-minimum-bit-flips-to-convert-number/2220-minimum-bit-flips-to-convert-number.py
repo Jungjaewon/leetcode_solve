d420fc5590207dc8e8f334b3e2e146398b92abd5
정재원 <Woodcook486@naver.com>
2024-01-02 13:29:42 +0900

    Time: 65 ms (5.41%), Space: 17.4 MB (5.98%) - LeetHub

--- 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.py
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.py
@@ -2,6 +2,7 @@ class Solution:
     def minBitFlips(self, start: int, goal: int) -> int:
         #https://leetcode.com/problems/minimum-bit-flips-to-convert-number/discuss/2260588/python-or-easy-or-interview-thinking
         #https://leetcode.com/problems/minimum-bit-flips-to-convert-number/discuss/2379732/One-Liner-Python
+        #https://www.educative.io/answers/what-is-kernighans-algorithm
         """
         self.ans = 10e4
         def func(cnt, idx, num, goal):
@@ -16,4 +17,10 @@ class Solution:
         func(0, 0, start, goal)
         return self.ans
         """
-        return (start^goal).bit_count()
+        #return (start^goal).bit_count()
+        res = start ^ goal
+        cnt = 0
+        while res:
+            res &= res - 1
+            cnt += 1
+        return cnt
