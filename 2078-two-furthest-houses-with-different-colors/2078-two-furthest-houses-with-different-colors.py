8c7cf772d19c081cc893d1fb3e35da6cf6466b53
정재원 <Woodcook486@naver.com>
2024-01-03 13:03:50 +0900

    Time: 95 ms (5.31%), Space: 17.4 MB (10.18%) - LeetHub

--- 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.py
+++ 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.py
@@ -11,9 +11,23 @@ class Solution:
                         ans = max(ans, abs(i - j))
         return ans
         """
+        """
         ans = -10e4
         for i in range(len(colors)):
             for j in range(i + 1, len(colors)):
                 if colors[i] != colors[j]:
                     ans = max(ans, abs(i - j))
         return ans
+        """
+        i,j, ans = 0, len(colors) - 1, -10e4
+        while i < j:
+            if colors[i] != colors[j]:
+                ans = max(ans, abs(i - j))
+            i += 1
+        i = 0
+        while i < j:
+            if colors[i] != colors[j]:
+                ans = max(ans, abs(i - j))
+            j -= 1
+        return ans
+        
