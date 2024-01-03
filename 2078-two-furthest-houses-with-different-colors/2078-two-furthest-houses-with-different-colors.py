57408ab4d71341cf989002235d4a6729e7b487f5
정재원 <Woodcook486@naver.com>
2024-01-03 12:54:16 +0900

    Time: 121 ms (5.31%), Space: 17.3 MB (16.59%) - LeetHub

--- 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.py
+++ 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.py
@@ -1,5 +1,6 @@
 class Solution:
     def maxDistance(self, colors: List[int]) -> int:
+        """
         ans = -10e4
         for i in range(len(colors)):
             for j in range(len(colors)):
@@ -8,4 +9,11 @@ class Solution:
                 else:
                     if colors[i] != colors[j]:
                         ans = max(ans, abs(i - j))
+        return ans
+        """
+        ans = -10e4
+        for i in range(len(colors)):
+            for j in range(i + 1, len(colors)):
+                if colors[i] != colors[j]:
+                    ans = max(ans, abs(i - j))
         return ans
