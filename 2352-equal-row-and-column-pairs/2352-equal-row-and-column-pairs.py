be770d00daa6cb23320400b4e0a17ab079571a11
정재원 <Woodcook486@naver.com>
2024-01-07 18:11:16 +0900

    Time: 3914 ms (5.01%), Space: 22.2 MB (28.76%) - LeetHub

---
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
@@ -0,0 +1,17 @@
+class Solution:
+    def equalPairs(self, grid: List[List[int]]) -> int:
+        ans = 0
+        col_list = list(zip(*grid))
+        for i in range(len(grid)):
+            for j in range(len(grid[i])):
+                temp_list1, temp_list2 = [], []
+                
+                for k in range(len(grid[i])):
+                    temp_list1.append(grid[i][k])
+                    
+                for k in range(len(grid[i])):
+                    temp_list2.append(grid[k][j])
+                #print(temp_list1, temp_list2)
+                if temp_list1 == temp_list2:
+                    ans += 1
+        return ans
