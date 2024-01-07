26ecd498bc2b89f7f92ba5bda94a2faae51973aa
정재원 <Woodcook486@naver.com>
2024-01-07 19:27:10 +0900

    Time: 610 ms (23.90%), Space: 22.3 MB (15.63%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
@@ -1,7 +1,7 @@
 class Solution:
     def equalPairs(self, grid: List[List[int]]) -> int:
+        """
         ans = 0
-        #col_list = list(zip(*grid))
         for i in range(len(grid)):
             for j in range(len(grid[i])):
                 temp_list1, temp_list2 = [], []
@@ -15,3 +15,12 @@ class Solution:
                 if temp_list1 == temp_list2:
                     ans += 1
         return ans
+        """
+        ans = 0
+        col_list = list(zip(*grid))
+        for item_a in grid:
+            for item_b in col_list:
+                if item_a == list(item_b):
+                    ans += 1
+        return ans
+        
