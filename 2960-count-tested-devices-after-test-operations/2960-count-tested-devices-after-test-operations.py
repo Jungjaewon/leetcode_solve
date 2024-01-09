ca452f3de795a1a76bc98cc707f504b956407653
정재원 <Woodcook486@naver.com>
2024-01-10 08:40:43 +0900

    Time: 99 ms (38.65%), Space: 17.4 MB (8.01%) - LeetHub

--- 2960-count-tested-devices-after-test-operations/2960-count-tested-devices-after-test-operations.py
+++ 2960-count-tested-devices-after-test-operations/2960-count-tested-devices-after-test-operations.py
@@ -1,5 +1,6 @@
 class Solution:
     def countTestedDevices(self, batteryPercentages: List[int]) -> int:
+        """
         ans = 0
         for i in range(len(batteryPercentages)):
             if batteryPercentages[i] > 0 :
@@ -7,5 +8,13 @@ class Solution:
                 for j in range(i + 1, len(batteryPercentages)):
                     if batteryPercentages[j] > 0 :
                         batteryPercentages[j] -= 1
+        return ans
+        """
+        ans = 0
+        for i in range(len(batteryPercentages)):
+            if batteryPercentages[i] > 0 :
+                ans += 1
+                for j in range(i + 1, len(batteryPercentages)):
+                    batteryPercentages[j] = max(0, batteryPercentages[j] - 1)
         return ans
             
