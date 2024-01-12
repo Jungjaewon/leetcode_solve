2511860d2239aff16b81965307d8310084dfdde2
정재원 <Woodcook486@naver.com>
2024-01-12 10:20:31 +0900

    Time: 96 ms (50.18%), Space: 17.8 MB (13.74%) - LeetHub

--- 2980-check-if-bitwise-or-has-trailing-zeros/2980-check-if-bitwise-or-has-trailing-zeros.py
+++ 2980-check-if-bitwise-or-has-trailing-zeros/2980-check-if-bitwise-or-has-trailing-zeros.py
@@ -1,5 +1,6 @@
 class Solution:
     def hasTrailingZeros(self, nums: List[int]) -> bool:
+        """
         for k in range(2, 3):
             for items in list(itertools.combinations(nums,k)):
                 temp, flag = 0, False
@@ -10,7 +11,22 @@ class Solution:
                     temp = temp | n                    
                 if flag:
                     continue
-                    
+                
                 if bin(temp)[-1:] == '0' or bin(temp)[-2:] == '10' or bin(temp)[-3:] == '110':
+                    return True
+        return False
+        """
+        for k in range(2, 3):
+            for items in list(itertools.combinations(nums,k)):
+                temp, flag = 0, False
+                for n in items:
+                    if n & 1 == 1:
+                        flag = True
+                        break
+                    temp = temp | n                    
+                if flag:
+                    continue
+                t = bin(temp)
+                if t[-1:] == '0' or t[-2:] == '10' or t[-3:] == '110':
                     return True
         return False
