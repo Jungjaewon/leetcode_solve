d0c25eaa76bf019045fd7bf3abb006515683629d
정재원 <Woodcook486@naver.com>
2024-01-21 21:18:12 +0900

    Time: 45 ms (28.04%), Space: 16.7 MB (60.42%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -3,7 +3,5 @@ class Solution:
         ans = [[]]
         for i in range(1, len(nums) + 1):
             temp = list(itertools.combinations(nums, i))
-            temp = [list(temp) for x in temp][0]
-            #print(temp)
             ans.extend(temp)
         return ans
