6b6f49c03ea5fcdd90c2659dd2cdf82f9bd8ffc8
정재원 <Woodcook486@naver.com>
2024-01-23 12:51:35 +0900

    Time: 37 ms (87.83%), Space: 16.6 MB (59.67%) - LeetHub

--- 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.py
+++ 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.py
@@ -1,6 +1,13 @@
 class Solution:
     def specialArray(self, nums: List[int]) -> int:
+        """
         for i in range(1, 1001):
+            base = [ x for x in nums if x >= i]
+            if len(base) == i:
+                return i
+        return -1
+        """
+        for i in range(1, 101):
             base = [ x for x in nums if x >= i]
             if len(base) == i:
                 return i
