583df8beb9f27abaeeac2420b58f7f3cbcf1d1c1
정재원 <Woodcook486@naver.com>
2024-01-23 12:50:54 +0900

    Time: 60 ms (18.17%), Space: 16.8 MB (57.33%) - LeetHub

---
+++ 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.py
@@ -0,0 +1,7 @@
+class Solution:
+    def specialArray(self, nums: List[int]) -> int:
+        for i in range(1, 1001):
+            base = [ x for x in nums if x >= i]
+            if len(base) == i:
+                return i
+        return -1
