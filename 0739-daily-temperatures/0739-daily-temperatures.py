0a7dc31ce68d3c15d03496c2a4553ca72cc2088f
정재원 <Woodcook486@naver.com>
2024-01-28 20:07:27 +0900

    Time: 904 ms (74.33%), Space: 32.1 MB (45.85%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -0,0 +1,27 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        """
+        ans = []
+        for i in range(len(temperatures)):
+            cnt, flag = 0, False
+            for j in range(i + 1, len(temperatures)):
+                if temperatures[i] < temperatures[j]:
+                    flag = True
+                    cnt += 1
+                    break
+                else:
+                    cnt += 1
+            ans.append(cnt if flag else 0)
+        return ans
+        """
+        ans = [0] * len(temperatures)  # Result array initialized with zeros
+        stack = []
+        # https://leetcode.com/problems/daily-temperatures/discuss/4630452/24.1-(Approach-1)-or-O(n)-or-Python-and-C%2B%2B(Step-by-step-explanation)
+        for i, t in enumerate(temperatures):
+            # While the stack is not empty and the current temperature is greater than the temperature at the top of the stack
+            while stack and t > stack[-1][0]:
+                temp_t, temp_i = stack.pop()
+                ans[temp_i] = i - temp_i  # Update the result for the popped index
+            stack.append([t, i])  # Push the current temperature and its index onto the stack
+
+        return ans  # Return the result array
