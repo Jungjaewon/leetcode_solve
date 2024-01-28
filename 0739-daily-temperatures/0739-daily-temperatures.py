c4d6aae100e7cc568466dffdcfd583ff1838208d
정재원 <Woodcook486@naver.com>
2024-01-28 20:15:56 +0900

    Time: 910 ms (69.63%), Space: 33.1 MB (15.22%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -14,6 +14,17 @@ class Solution:
             ans.append(cnt if flag else 0)
         return ans
         """
+        # https://leetcode.com/problems/daily-temperatures/discuss/1574808/C%2B%2BPython-3-Simple-Solutions-w-Explanation-Examples-and-Images-or-2-Monotonic-Stack-Approaches
+        """
+        ans = [0] * len(temperatures)
+        for i in range(len(temperatures)):
+            cnt, flag = 0, False
+            for j in range(i + 1, len(temperatures)):
+                if temperatures[i] < temperatures[j]:
+                    ans[i] = j - i
+                    break
+        return ans
+        """
         ans = [0] * len(temperatures)  # Result array initialized with zeros
         stack = []
         # https://leetcode.com/problems/daily-temperatures/discuss/4630452/24.1-(Approach-1)-or-O(n)-or-Python-and-C%2B%2B(Step-by-step-explanation)
@@ -23,5 +34,4 @@ class Solution:
                 temp_t, temp_i = stack.pop()
                 ans[temp_i] = i - temp_i  # Update the result for the popped index
             stack.append([t, i])  # Push the current temperature and its index onto the stack
-
         return ans  # Return the result array
