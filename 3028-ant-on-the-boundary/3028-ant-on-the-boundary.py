ed334a561c10df877149ddab4802dba1c4f51e44
정재원 <Woodcook486@naver.com>
2024-02-05 08:32:40 +0900

    Time: 44 ms (100.00%), Space: 16.5 MB (100.00%) - LeetHub

--- 3028-ant-on-the-boundary/3028-ant-on-the-boundary.py
+++ 3028-ant-on-the-boundary/3028-ant-on-the-boundary.py
@@ -1,10 +1,8 @@
 class Solution:
     def returnToBoundaryCount(self, nums: List[int]) -> int:
-        """
+        #nums = list(accumulate(nums))
+        #return len([1 for n in nums if n == 0])
         for i in range(1, len(nums)):
             nums[i] = nums[i] + nums[i - 1]
-        return len([1 for n in nums if n == 0])
-        """
-        nums = list(accumulate(nums))
         return len([1 for n in nums if n == 0])
         
