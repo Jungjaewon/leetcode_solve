abda2b06162da1e8363c4438f32c195012fc5554
정재원 <Woodcook486@naver.com>
2024-02-07 08:44:09 +0900

    Time: 55 ms (39.60%), Space: 16.7 MB (63.56%) - LeetHub

--- 0717-1-bit-and-2-bit-characters/0717-1-bit-and-2-bit-characters.py
+++ 0717-1-bit-and-2-bit-characters/0717-1-bit-and-2-bit-characters.py
@@ -1,5 +1,6 @@
 class Solution:
     def isOneBitCharacter(self, bits: List[int]) -> bool:
+        """
         while len(bits):
             if len(bits) < 2 and bits[0] == 0:
                 return True
@@ -15,5 +16,18 @@ class Solution:
                 else:
                     s = f'{bits[0]}{bits[1]}'
                     if s in ['10', '11']:
+                        bits = bits[2:]
+        return True
+        """
+        while len(bits):
+            if len(bits) < 2 and bits[0] == 0:
+                return True
+            elif len(bits) == 2:
+                return True if f'{bits[0]}{bits[1]}' == '00' else False
+            else:
+                if bits[0] == 0:
+                    bits = bits[1:]
+                else:
+                    if f'{bits[0]}{bits[1]}' in ['10', '11']:
                         bits = bits[2:]
         return True
