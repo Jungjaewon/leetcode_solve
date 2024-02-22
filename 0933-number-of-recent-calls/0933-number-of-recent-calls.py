7032d2441d4dcd86cc10eb11d85114692176f30b
정재원 <Woodcook486@naver.com>
2024-02-22 09:33:41 +0900

    Time: 186 ms (88.16%), Space: 22.2 MB (67.18%) - LeetHub

--- 0933-number-of-recent-calls/0933-number-of-recent-calls.py
+++ 0933-number-of-recent-calls/0933-number-of-recent-calls.py
@@ -2,6 +2,8 @@ class RecentCounter:
 
     def __init__(self):
         self.requests = list()
+        from collections import deque
+        self.q = deque()
     def ping(self, t: int) -> int:
         """
         self.requests.append(t)
@@ -12,9 +14,18 @@ class RecentCounter:
         self.requests.append(t)
         return cnt
         """
-        cnt = len([1 for x in self.requests if t - 3000 <= x and x <= t]) + 1
-        self.requests.append(t)
-        return cnt
+        #cnt = len([1 for x in self.requests if t - 3000 <= x and x <= t]) + 1
+        #self.requests.append(t)
+        #return cnt
+        
+        self.q.append(t)
+        
+        while t - self.q[0] > 3000:
+            self.q.popleft()
+            
+        return len(self.q)
+        
+        
         
 
 
