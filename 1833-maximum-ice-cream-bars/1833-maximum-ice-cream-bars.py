06e49574e86084eedd73a09790fff5811cc7733b
정재원 <Woodcook486@naver.com>
2024-02-25 13:52:39 +0900

    Time: 658 ms (45.80%), Space: 30.4 MB (81.98%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.py
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.py
@@ -1,11 +1,15 @@
 class Solution:
     def maxIceCream(self, costs: List[int], coins: int) -> int:
+        """
         costs, ans = sorted(costs), 0
         for c in costs:
             if c <= coins:
                 coins -= c
                 ans += 1
         return ans
+        """
+        # https://leetcode.com/problems/maximum-ice-cream-bars/discuss/3006066/One_liner.py
+        return sum(1 for icecream in sorted(costs) if (coins:= coins-icecream) >= 0)
             
         
         
