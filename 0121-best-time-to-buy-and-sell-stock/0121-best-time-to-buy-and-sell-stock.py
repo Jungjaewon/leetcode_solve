348beca82533a1f01d5e1b12ccd7c9004db13c01
정재원 <Woodcook486@naver.com>
2024-03-16 22:47:12 +0900

    Time: 755 ms (55.20%), Space: 27.4 MB (39.99%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,6 +1,7 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         #https://leetcode.com/problems/best-time-to-buy-and-sell-stock/discuss/1735550/Python-Javascript-Easy-solution-with-very-clear-Explanation
+        """
         left = 0 #Buy
         right = 1 #Sell
         max_profit = 0
@@ -12,4 +13,17 @@ class Solution:
                 left = right
             right += 1
         return max_profit
+        """
+        #https://leetcode.com/problems/best-time-to-buy-and-sell-stock/discuss/4801758/2-Steps-Kadane's-Algorithm-or-Brute-Better-Optimal-or-Easy-Video-Explanation-in-Hindi
+        n = len(prices)
+        maxProfit = 0
+        currMax_Profit = 0
+        for i in range(1, n):
+            currMax_Profit += prices[i] - prices[i-1]
+            if currMax_Profit > 0:
+                pass
+            else:
+                currMax_Profit = 0
+            maxProfit = max(maxProfit, currMax_Profit)
+        return maxProfit
                 
