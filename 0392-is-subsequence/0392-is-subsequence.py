aea196d1d6986359d65753c4a2f2aa2c4a7d6440
정재원 <Woodcook486@naver.com>
2024-03-17 16:29:52 +0900

    Time: 39 ms (34.38%), Space: 18.2 MB (5.69%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -1,5 +1,6 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
+        """
         if s == "":
             return True
         else:
@@ -15,4 +16,29 @@ class Solution:
                 return True
             else:
                 return False
-        
+        """
+        """
+        slow = 0
+        if len(s) == 0:
+            return True
+        for fast in range(len(t)):
+            if s[slow] == t[fast]:
+                slow += 1
+            if slow == len(s):
+                return True
+        return False
+        # https://leetcode.com/problems/is-subsequence/discuss/4738906/98-VERY-EASY-oror-Slow-and-Fast-pointer-solution
+        """
+        if s == "":
+            return True
+        else:
+            def func(s, t, s_l, t_l):
+                if s_l == -1:
+                    return True
+                if t_l == -1:
+                    return False
+                if s[s_l] == t[t_l]:
+                    return func(s, t, s_l -1, t_l -1)
+                else:
+                    return func(s, t, s_l, t_l -1)
+            return func(s, t, len(s) - 1, len(t) - 1)
