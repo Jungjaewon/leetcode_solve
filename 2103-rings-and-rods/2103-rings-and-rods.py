0fabcce729a05800c80ddc441245c109a5d837a2
정재원 <Woodcook486@naver.com>
2024-03-20 13:02:58 +0900

    Time: 28 ms (91.76%), Space: 16.5 MB (37.75%) - LeetHub

---
+++ 2103-rings-and-rods/2103-rings-and-rods.py
@@ -0,0 +1,6 @@
+class Solution:
+    def countPoints(self, rings: str) -> int:
+        d_dict = defaultdict(set)
+        for i in range(0, len(rings), 2):
+            d_dict[rings[i + 1]].add(rings[i])
+        return len([ 1 for x in d_dict if len(d_dict[x]) == 3])
