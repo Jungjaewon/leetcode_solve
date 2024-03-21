3b0b1e0e4048e963665482c2343e04fd54f98176
정재원 <Woodcook486@naver.com>
2024-03-22 08:49:00 +0900

    Time: 37 ms (43.92%), Space: 16.3 MB (99.41%) - LeetHub

---
+++ 2578-split-with-minimum-sum/2578-split-with-minimum-sum.py
@@ -0,0 +1,11 @@
+class Solution:
+    def splitNum(self, num: int) -> int:
+        num_list = [ x for x in list(str(num))]
+        num_list.sort(key=lambda x : int(x))
+        num1, num2 = list(), list()
+        for idx, n in enumerate(num_list):
+            if idx & 1 == 0:
+                num1.append(n)
+            else:
+                num2.append(n)
+        return int(''.join(num1)) + int(''.join(num2)) 
