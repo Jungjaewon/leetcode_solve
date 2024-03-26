94482041b833add6d7f2f5d7afb766cbe5c142cc
정재원 <Woodcook486@naver.com>
2024-03-26 09:04:48 +0900

    Time: 57 ms (57.89%), Space: 16.4 MB (99.37%) - LeetHub

---
+++ 1370-increasing-decreasing-string/1370-increasing-decreasing-string.py
@@ -0,0 +1,7 @@
+class Solution:
+    def sortString(self, s: str) -> str:
+        def key(c, phase={}):
+            p = phase[c] = phase.get(c, -1) + 1
+            return p, ord(c) * (-1)**p
+        return ''.join(sorted(s, key=key))
+    #https://leetcode.com/problems/increasing-decreasing-string/discuss/532293/4-lines-Python-sort-by-key
